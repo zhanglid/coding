8a623a34f6d0685231ee7ae28d564209f1594928
Zhangliang Dong <zhanglid@usc.edu>
2018-05-11 18:47:57 -0700

    rank和index没分清楚

--- src/main/java/com/zhangliang/leetcode/MedianOfTwoSortedArrays.java
+++ src/main/java/com/zhangliang/leetcode/MedianOfTwoSortedArrays.java
@@ -37,7 +37,7 @@ public class MedianOfTwoSortedArrays {
             return ((double)mid1 + mid2) / 2;
         }
 
-        return findTopKthInTwoArrays(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1, (len1 + len2) / 2);
+        return findTopKthInTwoArrays(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1, (len1 + len2) / 2 + 1);
     }
 
     private int findTopKthInTwoArrays(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
@@ -78,6 +78,6 @@ public class MedianOfTwoSortedArrays {
 
     public static void main(String[] args) {
         MedianOfTwoSortedArrays s = new MedianOfTwoSortedArrays();
-        System.out.println(s.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
+        System.out.println(s.findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
     }
 }
