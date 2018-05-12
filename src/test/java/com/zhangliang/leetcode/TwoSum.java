fedfbb07a6f6a6f96742958d464f3c949983f1d4
Zhangliang Dong <zhanglid@usc.edu>
2018-05-11 21:29:20 -0700

    vscode bug

--- src/test/java/com/zhangliang/leetcode/TwoSum.java
+++ src/test/java/com/zhangliang/leetcode/TwoSum.java
@@ -1,15 +1,15 @@
 package com.zhangliang.leetcode;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.junit.runners.Suite;
 
-@RunWith(Suite.class)
-public class TwoSum {
+// @RunWith(Suite.class)
+// public class TwoSum {
 
-    @Test
-    public void testDefault() {
-        System.out.println("heelo");
-    }
+    // @Test
+    // public void testDefault() {
+    //     System.out.println("heelo");
+    // }
 
-}
+// }
