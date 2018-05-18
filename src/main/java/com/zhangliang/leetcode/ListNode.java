bab4ebe172fd4fbf6ee55d5826fd8eafd6b444ae
Zhangliang Dong <zhanglid@usc.edu>
2018-05-18 00:57:46 -0700

    first

--- src/main/java/com/zhangliang/leetcode/ListNode.java
+++ src/main/java/com/zhangliang/leetcode/ListNode.java
@@ -7,4 +7,29 @@ public class ListNode {
     ListNode(int x) {
         val = x;
     }
+
+    ListNode(int[] nums) {
+        val = nums[0];
+        ListNode cur = this;
+        for (int i = 1; i < nums.length; i++) {
+            cur.next = new ListNode(nums[i]);
+            cur = cur.next;
+        }
+    }
+
+    public String toString() {
+        ListNode cur = next;
+        StringBuilder sb = new StringBuilder();
+        sb.append(val);
+        sb.append("->");
+
+        while (cur != null) {
+            sb.append(cur.val);
+            cur = cur.next;
+            if (cur != null)
+                sb.append("->");
+        }
+
+        return sb.toString();
+    }
 }
