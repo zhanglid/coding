c5184855194f34e8d71e01af5302bad4a12466e8
Zhangliang Dong <zhanglid@usc.edu>
2018-05-19 23:05:20 -0700

    first

---
+++ src/main/java/com/zhangliang/leetcode/SubstringWithConcatenationOfAllWords.java
@@ -0,0 +1,68 @@
+package com.zhangliang.leetcode;
+/*
+You are given a string, s, and a list of words, words, that are all of the same 
+length. Find all starting indices of substring(s) in s that is a concatenation 
+of each word in words exactly once and without any intervening characters.
+
+Example 1:
+
+Input:
+  s = "barfoothefoobarman",
+  words = ["foo","bar"]
+Output: [0,9]
+Explanation: Substrings starting at index 0 and 9 are "barfoor" and "foobar" 
+respectively.
+The output order does not matter, returning [9,0] is fine too.
+
+
+Example 2:
+
+Input:
+  s = "wordgoodstudentgoodword",
+  words = ["word","student"]
+Output: []
+*/
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SubstringWithConcatenationOfAllWords {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> ans = new ArrayList<>();
+        if (s == null || words == null || words.length < 1 || s.length() < words[0].length()) {
+            return ans;
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            map.put(words[i], i + 1);
+        }
+        int targetSum = (words.length + 1) * words.length / 2;
+        int step = words[0].length();
+        for (int offset = 0; offset < step; offset++) {
+            int sum = 0;
+            int i = offset;
+            while (i + step <= s.length()) {
+                sum += map.getOrDefault(s.substring(i, i + step), targetSum + 1);
+                if (sum == targetSum) {
+                    int start = i - step * (words.length - 1);
+                    ans.add(start);
+                    String first = s.substring(start, start + step);
+                    sum -= map.get(first);
+                } else if (sum > targetSum) {
+                    sum = 0;
+                }
+                i += step;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        SubstringWithConcatenationOfAllWords s = new SubstringWithConcatenationOfAllWords();
+        System.out.println(s.findSubstring("barfoothefoobarman", new String[] { "foo", "bar" }));
+    }
+}
---
+++ src/test/java/com/zhangliang/leetcode/SubstringWithConcatenationOfAllWordsTest.java
@@ -0,0 +1,30 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class SubstringWithConcatenationOfAllWordsTest {
+
+    @Test
+    public void testCase() {
+        SubstringWithConcatenationOfAllWords s = new SubstringWithConcatenationOfAllWords();
+        Integer[] ans = s.findSubstring("barfoothefoobarman", new String[] { "foo", "bar" }).toArray(new Integer[0]);
+        assertArrayEquals(ans, new Integer[] { 0, 9 });
+    }
+
+    @Test
+    public void testCase2() {
+        SubstringWithConcatenationOfAllWords s = new SubstringWithConcatenationOfAllWords();
+        Integer[] ans = s.findSubstring("wordgoodstudentgoodword", new String[] { "word", "student" })
+                .toArray(new Integer[0]);
+        assertArrayEquals(ans, new Integer[] {});
+    }
+
+    @Test
+    public void errCase() {
+        SubstringWithConcatenationOfAllWords s = new SubstringWithConcatenationOfAllWords();
+        Integer[] ans = s.findSubstring("aaaaaaaa", new String[] { "aa", "aa", "aa" }).toArray(new Integer[0]);
+        assertArrayEquals(ans, new Integer[] {});
+    }
+}
