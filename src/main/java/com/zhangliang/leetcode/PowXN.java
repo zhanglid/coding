840a18d9aaf6fe15b0581aac75846a1d395c122f
Zhangliang Dong <zhanglid@usc.edu>
2018-05-23 08:42:03 -0700

    first

---
+++ src/main/java/com/zhangliang/leetcode/PowXN.java
@@ -0,0 +1,46 @@
+package com.zhangliang.leetcode;
+/*
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+Example 3:
+
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+Note:
+
+-100.0 < x < 100.0
+n is a 32-bit signed integer, within the range [−231, 231 − 1]
+
+*/
+
+public class PowXN {
+    public double myPow(double x, int n) {
+        boolean reversed = false;
+        if (n < 0) {
+            reversed = true;
+            n = -n;
+        }
+
+        double ans = 1;
+        int base = 1;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                ans *= x;
+            }
+            n /= 2;
+            base = base * 2;
+            x *= x;
+        }
+
+        return reversed ? 1.0 / ans : ans;
+    }
+}
---
+++ src/test/java/com/zhangliang/leetcode/PowXNTest.java
@@ -0,0 +1,30 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class PowXNTest {
+
+    private static final double epsilon = 1e-6;
+    @Test
+    public void testCase() {
+        PowXN s = new PowXN();
+        double ans = s.myPow(2, 10);
+        assertEquals(1024.00, ans, Double.MIN_VALUE);
+    }
+
+    @Test
+    public void testCase2() {
+        PowXN s = new PowXN();
+        double ans = s.myPow(2.1, 3);
+        assertEquals(9.261, ans, epsilon);
+    }
+
+    @Test
+    public void testCase3() {
+        PowXN s = new PowXN();
+        double ans = s.myPow(2.0, -2);
+        assertEquals(0.25000, ans, Double.MIN_VALUE);
+    }
+}
