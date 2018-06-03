90c772288ab96555943ea7bc7e414c05a9098c7e
Zhangliang Dong <zhanglid@usc.edu>
2018-06-03 11:41:46 -0700

    arrays自带的binarysearch找到的话，就是该坐标。没找到会是插入位置，也就是之前的一个是最后一个小于该数的。

---
+++ src/main/java/com/zhangliang/leetcode/SearchA2DMatrix.java
@@ -0,0 +1,53 @@
+package com.zhangliang.leetcode;
+/*
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+Example 1:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+Example 2:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+Output: false
+*/
+
+import java.util.Arrays;
+
+public class SearchA2DMatrix {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) {
+            return false;
+        }
+
+        int[] firstCol = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            firstCol[i] = matrix[i][0];
+        }
+
+        int rowResult = Arrays.binarySearch(firstCol, target);
+
+        if (rowResult < 0) {
+            rowResult = -rowResult;
+            rowResult -= 2;
+        }
+
+        int colResult = Arrays.binarySearch(matrix[rowResult], target);
+        return colResult >= 0;
+    }
+}
---
+++ src/test/java/com/zhangliang/leetcode/SearchA2DMatrixTest.java
@@ -0,0 +1,28 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SearchA2DMatrixTest {
+
+    @Test
+    public void testCase() {
+        SearchA2DMatrix s = new SearchA2DMatrix();
+        boolean ans = s.searchMatrix(new int[][] { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 50 } }, 3);
+        assertEquals(true, ans);
+    }
+
+    @Test
+    public void testCase2() {
+        SearchA2DMatrix s = new SearchA2DMatrix();
+        boolean ans = s.searchMatrix(new int[][] { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 50 } }, 13);
+        assertEquals(false, ans);
+    }
+
+    @Test
+    public void errCase() {
+        SearchA2DMatrix s = new SearchA2DMatrix();
+        boolean ans = s.searchMatrix(new int[][] { { 1 } }, 1);
+        assertEquals(true, ans);
+    }
+}
