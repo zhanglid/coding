83083b7de1bfcc00483a801fc7dc2895478f947d
Zhangliang Dong <zhanglid@usc.edu>
2018-06-05 08:38:30 -0700

    first

---
+++ src/main/java/com/zhangliang/leetcode/WordSearch.java
@@ -0,0 +1,67 @@
+package com.zhangliang.leetcode;
+/*
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those 
+horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+Example:
+
+board =
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+
+Given word = "ABCCED", return true.
+Given word = "SEE", return true.
+Given word = "ABCB", return false.
+*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class WordSearch {
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length < 1 || board[0].length < 1 || word == null || word.isEmpty()) {
+            return false;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                Set<Integer> historyPos = new HashSet<>();
+                historyPos.add(i * board.length + j);
+                if (board[i][j] == word.charAt(0) && helper(board, word, 1, i, j, historyPos)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static final int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
+    private boolean helper(char[][] board, String word, int cur, int si, int sj, Set<Integer> historyPosSet) {
+        if (cur == word.length()) {
+            return true;
+        }
+
+        for (int[] dir : dirs) {
+            int i = si + dir[0];
+            int j = sj + dir[1];
+            int key = i * board.length + j;
+            if (i >= 0 && i < board.length && j >= 0 && j < board[0].length && !historyPosSet.contains(key)) {
+                if (board[i][j] == word.charAt(cur)) {
+                    historyPosSet.add(key);
+                    if (helper(board, word, cur + 1, i, j, historyPosSet)) {
+                        return true;
+                    }
+                    historyPosSet.remove(key);
+                }
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/com/zhangliang/leetcode/WordSearchTest.java
@@ -0,0 +1,31 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class WordSearchTest {
+
+    @Test
+    public void testCase() {
+        WordSearch s = new WordSearch();
+        boolean ans = s.exist(new char[][] { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } },
+                "ABCCED");
+        assertEquals(true, ans);
+    }
+
+    @Test
+    public void testCase2() {
+        WordSearch s = new WordSearch();
+        boolean ans = s.exist(new char[][] { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } },
+                "SEE");
+        assertEquals(true, ans);
+    }
+
+    @Test
+    public void testCase3() {
+        WordSearch s = new WordSearch();
+        boolean ans = s.exist(new char[][] { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } },
+                "ABCB");
+        assertEquals(false, ans);
+    }
+}
