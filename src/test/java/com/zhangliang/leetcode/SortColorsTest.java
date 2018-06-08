613c7a87ea9008a2fa1aaf03b3393492fd78079d
Zhangliang Dong <zhanglid@usc.edu>
2018-06-08 00:28:01 -0700

    first

---
+++ src/test/java/com/zhangliang/leetcode/SortColorsTest.java
@@ -0,0 +1,15 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+
+public class SortColorsTest {
+
+    @Test
+    public void testCase() {
+        SortColors s = new SortColors();
+        int[] input = new int[] { 2, 0, 2, 1, 1, 0 };
+        s.sortColors(input);
+        assertArrayEquals(new int[] { 0, 0, 1, 1, 2, 2 }, input);
+    }
+}
