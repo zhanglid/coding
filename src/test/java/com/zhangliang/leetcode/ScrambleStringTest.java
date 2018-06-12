6ca232af543f6f1f194d99391d035d4372f316dd
Zhangliang Dong <zhanglid@usc.edu>
2018-06-11 23:41:18 -0700

    first

---
+++ src/test/java/com/zhangliang/leetcode/ScrambleStringTest.java
@@ -0,0 +1,22 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class ScrambleStringTest {
+
+    @Test
+    public void testCase() {
+        ScrambleString s = new ScrambleString();
+        boolean ans = s.isScramble("great", "rgeat");
+        assertEquals(ans, true);
+    }
+
+    @Test
+    public void testCase2() {
+        ScrambleString s = new ScrambleString();
+        boolean ans = s.isScramble("abcde", "caebd");
+        assertEquals(ans, false);
+    }
+}
