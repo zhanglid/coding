34bb266868a1ec0be041873a9a367bb5e168e1a6
Zhangliang Dong <zhanglid@usc.edu>
2018-06-12 07:56:03 -0700

    first

--- src/main/java/com/zhangliang/leetcode/ScrambleString.java
+++ src/main/java/com/zhangliang/leetcode/ScrambleString.java
@@ -48,6 +48,28 @@ Output: false
 */
 
 public class ScrambleString {
+    private class Counter {
+        private int[] counts = new int[26];
+
+        public void add(char x) {
+            counts[x - 'a']++;
+        }
+
+        public int get(char x) {
+            return counts[x - 'a'];
+        }
+
+        public boolean equals(Counter other) {
+            for (char x = 'a'; x <= 'z'; x++) {
+                if (counts[x - 'a'] != other.get(x)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
     public boolean isScramble(String s1, String s2) {
         if (s1 == null || s2 == null) {
             return false;
@@ -61,17 +83,27 @@ public class ScrambleString {
             return true;
         }
 
+        Counter lcounter = new Counter();
+        Counter l2counter = new Counter();
+        Counter r2counter = new Counter();
+
         for (int i = 1; i < s1.length(); i++) {
             String l1 = s1.substring(0, i);
             String r1 = s1.substring(i);
-            String l2 = s2.substring(0, i);
-            String r2 = s2.substring(i);
+            String l2 = s2.substring(0, s1.length() - i);
+            String r2 = s2.substring(s1.length() - i);
+            String rl2 = s2.substring(0, i);
+            String rr2 = s2.substring(i);
 
-            if (isScramble(l1, l2) && isScramble(r1, r2)) {
+            lcounter.add(s1.charAt(i - 1));
+            l2counter.add(s2.charAt(i - 1));
+            r2counter.add(s2.charAt(s2.length() - i));
+
+            if (lcounter.equals(l2counter) && isScramble(l1, rl2) && isScramble(r1, rr2)) {
                 return true;
             }
 
-            if (isScramble(l1, r2) && isScramble(r1, l2)) {
+            if (lcounter.equals(r2counter) && isScramble(l1, r2) && isScramble(r1, l2)) {
                 return true;
             }
         }
--- src/test/java/com/zhangliang/leetcode/ScrambleStringTest.java
+++ src/test/java/com/zhangliang/leetcode/ScrambleStringTest.java
@@ -19,4 +19,18 @@ public class ScrambleStringTest {
         boolean ans = s.isScramble("abcde", "caebd");
         assertEquals(ans, false);
     }
+
+    @Test
+    public void errCase() {
+        ScrambleString s = new ScrambleString();
+        boolean ans = s.isScramble("abb", "bba");
+        assertEquals(ans, true);
+    }
+
+    @Test
+    public void errCase2() {
+        ScrambleString s = new ScrambleString();
+        boolean ans = s.isScramble("abcdefghijklmn", "efghijklmncadb");
+        assertEquals(ans, false);
+    }
 }
