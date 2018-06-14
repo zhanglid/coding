9ee2d1fb01f67a757d1f09a922c1ca353cefeba8
Zhangliang Dong <zhanglid@usc.edu>
2018-06-14 07:40:47 -0700

    first

---
+++ src/main/java/com/zhangliang/leetcode/DecodeWays.java
@@ -0,0 +1,41 @@
+package com.zhangliang.leetcode;
+/*
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given a non-empty string containing only digits, determine the total number of ways to decode it.
+
+Example 1:
+
+Input: "12"
+Output: 2
+Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+Example 2:
+
+Input: "226"
+Output: 3
+Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+*/
+
+public class DecodeWays {
+    public int numDecodings(String s) {
+        if (s == null || s.length() < 1) {
+            return 1;
+        }
+
+        char[] chars = s.toCharArray();
+        int[] dp = new int[chars.length + 1];
+        dp[0] = 1;
+        for (int i = 0; i < chars.length; i++) {
+            dp[i + 1] = dp[i];
+            if (i - 1 >= 0 && (chars[i - 1] != '0' && (10 * (chars[i - 1] - '0') + chars[i] - '0' <= 26))) {
+                dp[i + 1] += dp[i - 1];
+            }
+        }
+
+        return dp[chars.length];
+    }
+}
---
+++ src/test/java/com/zhangliang/leetcode/DecodeWaysTest.java
@@ -0,0 +1,22 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class DecodeWaysTest {
+
+    @Test
+    public void testCase() {
+        DecodeWays s = new DecodeWays();
+        int ans = s.numDecodings("12");
+        assertEquals(2, ans);
+    }
+
+    @Test
+    public void testCase2() {
+        DecodeWays s = new DecodeWays();
+        int ans = s.numDecodings("226");
+        assertEquals(3, ans);
+    }
+}
