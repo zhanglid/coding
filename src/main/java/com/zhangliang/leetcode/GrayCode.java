36ad3218936e141ce3131f91e4944c2799bc5bae
Zhangliang Dong <zhanglid@usc.edu>
2018-06-14 08:32:57 -0700

    first

---
+++ src/main/java/com/zhangliang/leetcode/GrayCode.java
@@ -0,0 +1,46 @@
+package com.zhangliang.leetcode;
+/*
+The gray code is a binary numeral system where two successive values differ in only one bit.
+
+Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray 
+code. A gray code sequence must begin with 0.
+
+For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+
+00 - 0
+01 - 1
+11 - 3
+10 - 2
+Note:
+For a given n, a gray code sequence is not uniquely defined.
+
+For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+
+For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.
+
+
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GrayCode {
+    public List<Integer> grayCode(int n) {
+        List<Integer> ans = new ArrayList<>();
+        if (n < 1) {
+            return ans;
+        }
+        ans.add(0);
+
+        int delta = 1;
+        for (int i = 1; i <= n; i++) {
+            int size = ans.size();
+            for (int j = size - 1; j >= 0; j--) {
+                ans.add(ans.get(j) + delta);
+            }
+            delta *= 2;
+        }
+
+        return ans;
+    }
+}
---
+++ src/test/java/com/zhangliang/leetcode/GrayCodeTest.java
@@ -0,0 +1,16 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertArrayEquals;
+import java.util.List;
+
+import org.junit.Test;
+
+public class GrayCodeTest {
+
+    @Test
+    public void testCase() {
+        GrayCode s = new GrayCode();
+        List<Integer> ans = s.grayCode(2);
+        assertArrayEquals(new Integer[] { 0, 1, 3, 2 }, ans.toArray(new Integer[0]));
+    }
+}
