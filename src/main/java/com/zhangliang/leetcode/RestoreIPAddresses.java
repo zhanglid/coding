a7d8513110c4f59b920ab4095dd0907ef39696c4
Zhangliang Dong <zhanglid@usc.edu>
2018-06-14 06:24:17 -0700

    first

---
+++ src/main/java/com/zhangliang/leetcode/RestoreIPAddresses.java
@@ -0,0 +1,47 @@
+package com.zhangliang.leetcode;
+/*
+Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+Example:
+
+Input: "25525511135"
+Output: ["255.255.11.135", "255.255.111.35"]
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestoreIPAddresses {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> ans = new ArrayList<>();
+        if (s == null) {
+            return ans;
+        }
+
+        helper(s, 0, new String[4], 0, ans);
+
+        return ans;
+    }
+
+    private void helper(String s, int start, String[] parts, int cur, List<String> ans) {
+        int part = 0;
+        if (cur == 3) {
+            part = Integer.parseInt(s.substring(start));
+            if (part <= 255) {
+                parts[cur++] = Integer.toString(part);
+                ans.add(String.join(".", parts));
+            }
+            return;
+        }
+
+        part = 0;
+        for (int i = start; i < Math.min(i + 3, s.length() - (4 - cur - 1)); i++) {
+            part = 10 * part + (s.charAt(i) - '0');
+            if (part > 255) {
+                return;
+            }
+            parts[cur] = Integer.toString(part);
+            helper(s, i + 1, parts, cur + 1, ans);
+        }
+    }
+}
---
+++ src/test/java/com/zhangliang/leetcode/RestoreIPAddressesTest.java
@@ -0,0 +1,18 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertArrayEquals;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+public class RestoreIPAddressesTest {
+
+    @Test
+    public void testCase() {
+        RestoreIPAddresses s = new RestoreIPAddresses();
+        List<String> ans = s.restoreIpAddresses("25525511135");
+        Collections.sort(ans);
+        assertArrayEquals(new String[] { "255.255.11.135", "255.255.111.35" }, ans.toArray(new String[0]));
+    }
+}
