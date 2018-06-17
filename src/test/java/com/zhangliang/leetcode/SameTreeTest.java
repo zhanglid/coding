0ba98a1f675eae76c79c0a9c5fbfee08fa0fa434
Zhangliang Dong <zhanglid@usc.edu>
2018-06-17 15:46:38 -0700

    first

---
+++ src/test/java/com/zhangliang/leetcode/SameTreeTest.java
@@ -0,0 +1,25 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+
+import com.zhangliang.utils.TreeNode;
+
+import org.junit.Test;
+
+public class SameTreeTest {
+
+    @Test
+    public void testCase() {
+        SameTree s = new SameTree();
+        boolean ans = s.isSameTree(new TreeNode(new Integer[] { 1, 2, 3 }), new TreeNode(new Integer[] { 1, 2, 3 }));
+        assertEquals(true, ans);
+    }
+
+    @Test
+    public void testCase2() {
+        SameTree s = new SameTree();
+        boolean ans = s.isSameTree(new TreeNode(new Integer[] { 1, 2, 1 }), new TreeNode(new Integer[] { 1, 1, 2 }));
+        assertEquals(false, ans);
+    }
+}
