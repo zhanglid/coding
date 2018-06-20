7a9d79257cc0d7870fcf7faaec689e65f80a5eb2
Zhangliang Dong <zhanglid@usc.edu>
2018-06-19 23:35:21 -0700

    first

---
+++ src/test/java/com/zhangliang/leetcode/BestTimeToBuyAndSellStockTest.java
@@ -0,0 +1,22 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class BestTimeToBuyAndSellStockTest {
+
+    @Test
+    public void testCase() {
+        BestTimeToBuyAndSellStock s = new BestTimeToBuyAndSellStock();
+        int ans = s.maxProfit(new int[] { 7, 1, 5, 3, 6, 4 });
+        assertEquals(5, ans);
+    }
+
+    @Test
+    public void testCase2() {
+        BestTimeToBuyAndSellStock s = new BestTimeToBuyAndSellStock();
+        int ans = s.maxProfit(new int[] { 7, 6, 4, 3, 1 });
+        assertEquals(0, ans);
+    }
+}
