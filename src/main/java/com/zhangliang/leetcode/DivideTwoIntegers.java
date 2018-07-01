f17e3c8b273ddb27f651b0771527a6d3b8e695cd
Zhangliang Dong <zhanglid@usc.edu>
2018-06-30 18:50:51 -0700

    review

--- src/main/java/com/zhangliang/leetcode/DivideTwoIntegers.java
+++ src/main/java/com/zhangliang/leetcode/DivideTwoIntegers.java
@@ -27,56 +27,50 @@ result overflows.
 
 public class DivideTwoIntegers {
     public int divide(int dividend, int divisor) {
-        boolean sign = (dividend < 0 && divisor < 0) || (dividend > 0 && divisor > 0);
-
-        if (dividend < 0) {
+        if (divisor == Integer.MIN_VALUE) {
             if (dividend == Integer.MIN_VALUE) {
-                if (sign && divisor == -1) {
-                    return Integer.MAX_VALUE;
-                }
-                
-                if (divisor < 0) {
-                    return divide(dividend - divisor, divisor) + 1;
-                }
-                else {
-                    return divide(dividend + divisor, divisor) - 1;
-                }
+                return 1;
             }
-            dividend = -dividend;
-        }
 
-        if (divisor < 0) {
-            if (divisor == Integer.MIN_VALUE) {
-                return 0;
-            }
-            divisor = -divisor;
-        }
-
-        if (dividend < divisor) {
             return 0;
         }
-        int base = 1;
-        while (dividend - divisor >=  divisor) {
-            divisor = divisor << 1;
-            base = base << 1;
+
+        int offset = 0;
+        if (dividend == Integer.MIN_VALUE) {
+            dividend += Math.abs(divisor);
+            offset += 1;
+        }
+        boolean isPostive = (dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0);
+
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+
+        int base = divisor;
+        int baseIndex = 1;
+
+        while (base * 2 < dividend && base * 2 > base) {
+            base *= 2;
+            baseIndex *= 2;
         }
 
         int ans = 0;
-        while (base >= 1) {
-            ans = ans + ans;
-            if (dividend >= divisor) {
-                ans += 1;
-                dividend -= divisor;
+        while (dividend >= divisor) {
+            if (dividend >= base) {
+                dividend -= base;
+                ans += baseIndex;
             }
-            divisor = divisor >> 1;
             base = base >> 1;
+            baseIndex = baseIndex >> 1;
         }
 
-        return sign ? ans : -ans;
-    }
-
-    public static void main(String[] args) {
-        DivideTwoIntegers s = new DivideTwoIntegers();
-        System.out.println(s.divide(2147483647, 3));
+        if (ans + offset < 0) {
+            if (isPostive) {
+                return Integer.MAX_VALUE;
+            } else {
+                return Integer.MIN_VALUE;
+            }
+        }
+        ans += offset;
+        return isPostive ? ans : -ans;
     }
 }
--- src/test/java/com/zhangliang/leetcode/DivideTwoIntegersTest.java
+++ src/test/java/com/zhangliang/leetcode/DivideTwoIntegersTest.java
@@ -9,48 +9,48 @@ public class DivideTwoIntegersTest {
     public void testCase() {
         DivideTwoIntegers s = new DivideTwoIntegers();
         int ans = s.divide(10, 3);
-        assertEquals(ans, 3);
+        assertEquals(3, ans);
     }
 
     @Test
     public void testCase2() {
         DivideTwoIntegers s = new DivideTwoIntegers();
         int ans = s.divide(7, -3);
-        assertEquals(ans, -2);
+        assertEquals(-2, ans);
     }
 
     @Test
     public void errCase() {
         DivideTwoIntegers s = new DivideTwoIntegers();
         int ans = s.divide(1, 1);
-        assertEquals(ans, 1);
+        assertEquals(1, ans);
     }
 
     @Test
     public void errCase2() {
         DivideTwoIntegers s = new DivideTwoIntegers();
         int ans = s.divide(2147483647, 1);
-        assertEquals(ans, 2147483647);
+        assertEquals(2147483647, ans);
     }
 
     @Test
     public void errCase3() {
         DivideTwoIntegers s = new DivideTwoIntegers();
         int ans = s.divide(-2147483648, 1);
-        assertEquals(ans, -2147483648);
+        assertEquals(-2147483648, ans);
     }
 
     @Test
     public void errCase4() {
         DivideTwoIntegers s = new DivideTwoIntegers();
         int ans = s.divide(2147483647, 3);
-        assertEquals(ans, 715827882);
+        assertEquals(715827882, ans);
     }
 
     @Test
     public void errCase5() {
         DivideTwoIntegers s = new DivideTwoIntegers();
         int ans = s.divide(-1010369383, -2147483648);
-        assertEquals(ans, 0);
+        assertEquals(0, ans);
     }
 }
