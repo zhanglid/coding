bf4bc231c0f12e948f89ad0c175386dc057fa638
Zhangliang Dong <zhanglid@usc.edu>
2018-06-30 18:08:29 -0700

    review

--- src/main/java/com/zhangliang/leetcode/ScrambleString.java
+++ src/main/java/com/zhangliang/leetcode/ScrambleString.java
@@ -48,63 +48,55 @@ Output: false
 */
 
 public class ScrambleString {
-    private class Counter {
-        private int[] counts = new int[26];
-
-        public void add(char x) {
-            counts[x - 'a']++;
-        }
-
-        public int get(char x) {
-            return counts[x - 'a'];
-        }
-
-        public boolean equals(Counter other) {
-            for (char x = 'a'; x <= 'z'; x++) {
-                if (counts[x - 'a'] != other.get(x)) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-    }
-
     public boolean isScramble(String s1, String s2) {
-        if (s1 == null || s2 == null) {
-            return false;
-        }
-
-        if (s1.length() != s2.length()) {
+        if (s1 == null || s2 == null || s1.length() != s2.length()) {
             return false;
         }
 
         if (s1.equals(s2)) {
             return true;
         }
+        int[] counter1 = new int[26];
+        int[] counter2 = new int[26];
+        int[] counter3 = new int[26];
 
-        Counter lcounter = new Counter();
-        Counter l2counter = new Counter();
-        Counter r2counter = new Counter();
+        for (int i = 0; i < s1.length() - 1; i++) {
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+            char c3 = s2.charAt(s2.length() - i - 1);
 
-        for (int i = 1; i < s1.length(); i++) {
-            String l1 = s1.substring(0, i);
-            String r1 = s1.substring(i);
-            String l2 = s2.substring(0, s1.length() - i);
-            String r2 = s2.substring(s1.length() - i);
-            String rl2 = s2.substring(0, i);
-            String rr2 = s2.substring(i);
+            counter1[c1 - 'a']++;
+            counter2[c2 - 'a']++;
+            counter3[c3 - 'a']++;
 
-            lcounter.add(s1.charAt(i - 1));
-            l2counter.add(s2.charAt(i - 1));
-            r2counter.add(s2.charAt(s2.length() - i));
-
-            if (lcounter.equals(l2counter) && isScramble(l1, rl2) && isScramble(r1, rr2)) {
-                return true;
+            boolean isDifference12 = false;
+            boolean isDifference13 = false;
+            for (int j = 0; j < 26; j++) {
+                if (counter1[j] != counter2[j]) {
+                    isDifference12 = true;
+                    break;
+                }
             }
 
-            if (lcounter.equals(r2counter) && isScramble(l1, r2) && isScramble(r1, l2)) {
-                return true;
+            for (int j = 0; j < 26; j++) {
+                if (counter1[j] != counter3[j]) {
+                    isDifference13 = true;
+                    break;
+                }
+            }
+
+            if (!isDifference12) {
+                if (isScramble(s1.substring(0, i + 1), s2.substring(0, i + 1))
+                        && isScramble(s1.substring(i + 1), s2.substring(i + 1))) {
+                    return true;
+                }
+            }
+
+            if (!isDifference13) {
+                if (isScramble(s1.substring(0, i + 1), s2.substring(s2.length() - i - 1))
+                        && isScramble(s1.substring(i + 1), s2.substring(0, s2.length() - i - 1))) {
+                    return true;
+                }
             }
         }
 
