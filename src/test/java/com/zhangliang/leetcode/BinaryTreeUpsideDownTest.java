9721b5ad1dafb11359cfdea3957682a69835b55e
Zhangliang Dong <zhanglid@usc.edu>
2018-07-03 07:37:54 -0700

    first

---
+++ src/test/java/com/zhangliang/leetcode/BinaryTreeUpsideDownTest.java
@@ -0,0 +1,21 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import com.zhangliang.utils.TreeNode;
+
+import org.junit.Test;
+
+public class BinaryTreeUpsideDownTest {
+
+    @Test
+    public void testCase() {
+        BinaryTreeUpsideDown s = new BinaryTreeUpsideDown();
+        TreeNode ans = s.upsideDownBinaryTree(new TreeNode(new Integer[] { 1, 2, 3, 4, 5 }));
+        TreeNode expected = new TreeNode(new Integer[] { 4, 5, 2, null, null, 3, 1 });
+        assertTrue(expected.equals(ans));
+    }
+}
