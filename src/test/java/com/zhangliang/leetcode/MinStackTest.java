0b03665365b2a23dfd63607811c208347a5394b7
Zhangliang Dong <zhanglid@usc.edu>
2018-07-03 08:47:09 -0700

    first

---
+++ src/test/java/com/zhangliang/leetcode/MinStackTest.java
@@ -0,0 +1,15 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class MinStackTest {
+
+    @Test
+    public void testCase() {
+        MinStack s = new MinStack();
+        String ans = s.solve();
+        assertEquals(ans, "");
+    }
+}
