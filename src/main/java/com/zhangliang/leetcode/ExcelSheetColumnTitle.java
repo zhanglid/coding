076385a31ded445b71a66126823951a113842630
Zhangliang Dong <zhanglid@usc.edu>
2018-07-05 22:51:02 -0700

    first

---
+++ src/main/java/com/zhangliang/leetcode/ExcelSheetColumnTitle.java
@@ -0,0 +1,50 @@
+package com.zhangliang.leetcode;
+/*
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+    ...
+Example 1:
+
+Input: 1
+Output: "A"
+Example 2:
+
+Input: 28
+Output: "AB"
+Example 3:
+
+Input: 701
+Output: "ZY"
+*/
+
+public class ExcelSheetColumnTitle {
+    public String convertToTitle(int n) {
+        if (n < 1) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+
+        int base = 1;
+        while (n > 0) {
+            int digit = n % 26;
+            if (digit == 0) {
+                digit = 26;
+            }
+            sb.insert(0, (char) (digit - 1 + 'A'));
+            n -= digit * base;
+            n /= 26;
+            base *= 26;
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/zhangliang/leetcode/ExcelSheetColumnTitleTest.java
@@ -0,0 +1,28 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ExcelSheetColumnTitleTest {
+
+    @Test
+    public void testCase() {
+        ExcelSheetColumnTitle s = new ExcelSheetColumnTitle();
+        String ans = s.convertToTitle(1);
+        assertEquals("A", ans);
+    }
+
+    @Test
+    public void testCase2() {
+        ExcelSheetColumnTitle s = new ExcelSheetColumnTitle();
+        String ans = s.convertToTitle(28);
+        assertEquals("AB", ans);
+    }
+
+    @Test
+    public void testCase3() {
+        ExcelSheetColumnTitle s = new ExcelSheetColumnTitle();
+        String ans = s.convertToTitle(701);
+        assertEquals("ZY", ans);
+    }
+}
