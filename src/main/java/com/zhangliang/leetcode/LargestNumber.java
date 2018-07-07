fba5148ad004e9bdae4ff8805d841c04910ce0c5
Zhangliang Dong <zhanglid@usc.edu>
2018-07-07 10:35:19 -0700

    first

---
+++ src/main/java/com/zhangliang/leetcode/LargestNumber.java
@@ -0,0 +1,45 @@
+package com.zhangliang.leetcode;
+/*
+Given a list of non negative integers, arrange them such that they form the largest number.
+
+Example 1:
+
+Input: [10,2]
+Output: "210"
+Example 2:
+
+Input: [3,30,34,5,9]
+Output: "9534330"
+Note: The result may be very large, so you need to return a string instead of an integer.
+*/
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LargestNumber {
+    public String largestNumber(int[] nums) {
+        if (nums == null || nums.length < 1) {
+            return "";
+        }
+
+        Integer[] converted = new Integer[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            converted[i] = nums[i];
+        }
+
+        Arrays.sort(converted, new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                String bStr = b.toString();
+                String aStr = a.toString();
+                return (bStr + aStr).compareTo(aStr + bStr);
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (Integer value : converted) {
+            sb.append(value.toString());
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/zhangliang/leetcode/LargestNumberTest.java
@@ -0,0 +1,28 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class LargestNumberTest {
+
+    @Test
+    public void testCase() {
+        LargestNumber s = new LargestNumber();
+        String ans = s.largestNumber(new int[] { 10, 2 });
+        assertEquals("210", ans);
+    }
+
+    @Test
+    public void testCase2() {
+        LargestNumber s = new LargestNumber();
+        String ans = s.largestNumber(new int[] { 3, 30, 34, 5, 9 });
+        assertEquals("9534330", ans);
+    }
+
+    @Test
+    public void errCase() {
+        LargestNumber s = new LargestNumber();
+        String ans = s.largestNumber(new int[] { 121, 12 });
+        assertEquals("12121", ans);
+    }
+}
