b2cb890f995d33db640dfe203436062b8e9f1624
Zhangliang <dzl199401@126.com>
2018-08-10 07:08:06 -0700

    first

---
+++ src/test/java/com/zhangliang/leetcode/RectangleOverlapTest.java
@@ -0,0 +1,22 @@
+package com.zhangliang.leetcode;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class RectangleOverlapTest {
+
+    @Test
+    public void testCase() {
+        RectangleOverlap s = new RectangleOverlap();
+        boolean ans = s.isRectangleOverlap(new int[] { 0, 0, 2, 2 }, new int[] { 1, 1, 3, 3 });
+        assertEquals(true, ans);
+    }
+
+    @Test
+    public void testCase2() {
+        RectangleOverlap s = new RectangleOverlap();
+        boolean ans = s.isRectangleOverlap(new int[] { 0, 0, 1, 1 }, new int[] { 1, 0, 2, 1 });
+        assertEquals(false, ans);
+    }
+}
