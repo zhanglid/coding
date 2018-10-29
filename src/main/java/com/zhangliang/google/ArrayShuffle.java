ef0eff7a6909d28739d71060e7e0d635f21b8385
Zhangliang <dzl199401@126.com>
2018-10-28 22:10:55 -0700

    first

---
+++ src/main/java/com/zhangliang/google/ArrayShuffle.java
@@ -0,0 +1,36 @@
+package com.zhangliang.google;
+/*
+数组1是排好序的1-n个数字，数组2是根据数组1 shuffle得出，给定数组3，要求根据一样的 shuffle规则变换成
+数组4，输出数组4。
+第一个follow up，如果数组一是无重复数字组成的无序数组，如何做
+第二个follow up，数组1有重复。
+假如arr1 = {1, 2, 3, 4}, arr2 = {4, 2, 1, 3}
+首先需要预处理arr2，得到val2NewIndex map 然后遍历arr1，新建index2NewIndex，那么index2NewIndex.
+put(i, val2NewIndex.get(a[i])) 对于arr3，根据index2NewIndex逐个填写元素到arr4中即可。
+Follow-up 1
+跟上面做法相同
+Follow-up 2
+将val2NewIndex map的每个值变成List<Integer>就行了
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayShuffle {
+    public int[] solve(int[] arr1, int[] arr2, int[] arr3) {
+        int[] ans = new int[arr1.length];
+        Map<Integer, Integer> value2Index = new HashMap<>();
+        for (int i = 0; i < arr2.length; i++) {
+            value2Index.put(arr2[i], i);
+        }
+        Map<Integer, Integer> index2index = new HashMap<>();
+        for (int i = 0; i < arr1.length; i++) {
+            index2index.put(i, value2Index.get(arr1[i]));
+        }
+        for (int i = 0; i < arr3.length; i++) {
+            int newIndex = index2index.get(i);
+            ans[newIndex] = arr3[i];
+        }
+        return ans;
+    }
+}
---
+++ src/test/java/com/zhangliang/google/ArrayShuffleTest.java
@@ -0,0 +1,20 @@
+package com.zhangliang.google;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class ArrayShuffleTest {
+
+    @Test
+    public void testCase() {
+        ArrayShuffle s = new ArrayShuffle();
+        int[] arr1 = new int[] { 1, 2, 3, 4 };
+        int[] arr2 = new int[] { 4, 2, 1, 3 };
+        int[] arr3 = new int[] { 4, 3, 2, 1 };
+        int[] arr4 = new int[] { 1, 3, 4, 2 };
+        int[] ans = s.solve(arr1, arr2, arr3);
+        assertArrayEquals(arr4, ans);
+    }
+}
