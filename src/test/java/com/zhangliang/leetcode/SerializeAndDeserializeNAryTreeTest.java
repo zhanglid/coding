bfb4a9ac829df03cc5b1f2e8ddab53d120ae9626
Zhangliang <dzl199401@126.com>
2019-04-09 21:24:38 -0700

    Serialize and Deserialize N-ary Tree

--- src/test/java/com/zhangliang/leetcode/SerializeAndDeserializeNAryTreeTest.java
+++ src/test/java/com/zhangliang/leetcode/SerializeAndDeserializeNAryTreeTest.java
@@ -1,6 +1,8 @@
 package com.zhangliang.leetcode;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import com.zhangliang.leetcode.SerializeAndDeserializeNAryTree.Node;
@@ -12,14 +14,24 @@ public class SerializeAndDeserializeNAryTreeTest {
 
     @Test
     public void testCase() {
-        Node root = new Node(1, null);
         Codec coder = new Codec();
-        root.children.add(new Node(3, Arrays.asList(new Node[] { new Node(5, null), new Node(6, null) })));
-        root.children.add(new Node(2, null));
-        root.children.add(new Node(4, null));
+        Node root = new Node(1, new ArrayList<>());
+        root.children.add(new Node(3,
+                Arrays.asList(new Node[] { new Node(5, new ArrayList<>()), new Node(6, new ArrayList<>()) })));
+        root.children.add(new Node(2, new ArrayList<>()));
+        root.children.add(new Node(4, new ArrayList<>()));
 
         String code = coder.serialize(root);
         Node newRoot = coder.deserialize(code);
         assertEquals(true, newRoot.isEqual(root));
+    }
+
+    @Test
+    public void testCase2() {
+        Codec coder = new Codec();
+        Node root = null;
+        String code = coder.serialize(root);
+        Node newRoot = coder.deserialize(code);
+        assertEquals(null, newRoot);
     }
 }
