28053f6e15549acac906fb14ec1bbc2d1b332f69
Zhangliang <dzl199401@126.com>
2019-05-06 23:01:34 -0700

    update

--- src/main/java/com/zhangliang/leetcode/RestoreIPAddresses.java
+++ src/main/java/com/zhangliang/leetcode/RestoreIPAddresses.java
@@ -14,47 +14,29 @@ import java.util.List;
 public class RestoreIPAddresses {
     public List<String> restoreIpAddresses(String s) {
         List<String> ans = new ArrayList<>();
-        if (s == null) {
-            return ans;
-        }
-
-        helper(s, 0, new String[4], 0, ans);
-
+        helper(new ArrayList<>(), 0, s, ans);
         return ans;
     }
 
-    private void helper(String s, int start, String[] parts, int cur, List<String> ans) {
-        int part = 0;
-        if (start >= s.length()) {
-            return;
-        }
-        if (s.length() - start > 3 * (4 - cur)) {
-            return;
-        }
-        if (cur == 3) {
-            String last = s.substring(start);
-            part = Integer.parseInt(last);
-            if (part <= 255 && (part == 0 && last.equals("0") || part > 0 && s.charAt(start) != '0')) {
-                parts[cur++] = Integer.toString(part);
-                ans.add(String.join(".", parts));
+    private void helper(List<String> parts, int index, String s, List<String> result) {
+        if (parts.size() == 4) {
+            if (index >= s.length()) {
+                result.add(String.join(".", parts));
             }
             return;
         }
-
-        if (s.charAt(start) == '0') {
-            parts[cur] = "0";
-            helper(s, start + 1, parts, cur + 1, ans);
-            return;
-        }
-
-        part = 0;
-        for (int i = start; i < Math.min(i + 3, s.length() - (4 - cur - 1)); i++) {
+        int part = 0;
+        for (int i = index; i < Math.min(s.length(), index + 3); i++) {
             part = 10 * part + (s.charAt(i) - '0');
             if (part > 255) {
-                return;
+                break;
             }
-            parts[cur] = Integer.toString(part);
-            helper(s, i + 1, parts, cur + 1, ans);
+            parts.add(Integer.toString(part));
+            helper(parts, i + 1, s, result);
+            parts.remove(parts.size() - 1);
+            if (part == 0) {
+                break;
+            }
         }
     }
 }
