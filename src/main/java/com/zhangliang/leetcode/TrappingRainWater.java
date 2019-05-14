3fc460e2021f21478927933aa3596d9d4a507b32
Zhangliang <dzl199401@126.com>
2019-05-13 22:47:33 -0700

    Update

--- src/main/java/com/zhangliang/leetcode/TrappingRainWater.java
+++ src/main/java/com/zhangliang/leetcode/TrappingRainWater.java
@@ -7,33 +7,35 @@ each bar is 1, compute how much water it is able to trap after raining.
 The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In 
 this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos 
 for contributing this image!
-
+       1
+   1xxx11x1
+ 1x11x111111
 Example:
 
 Input: [0,1,0,2,1,0,1,3,2,1,2,1]
 Output: 6
 */
 
+import java.util.*;
+
 public class TrappingRainWater {
     public int trap(int[] height) {
-        if (height == null || height.length < 1) {
-            return 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for (int val : height) {
+            if (stack.peek() <= val) {
+                stack.push(val);
+            }
         }
-
-        int[] leftMaxes = new int[height.length];
-        int max = 0;
-        for (int i = 0; i < height.length; i++) {
-            leftMaxes[i] = max;
-            max = Math.max(max, height[i]);
-        }
-
+        int rightMax = 0;
         int ans = 0;
-        max = 0;
         for (int i = height.length - 1; i >= 0; i--) {
-            ans += Math.max(0, Math.min(max, leftMaxes[i]) - height[i]);
-            max = Math.max(max, height[i]);
+            ans += Math.max(0, Math.min(stack.peek(), rightMax) - height[i]);
+            rightMax = Math.max(rightMax, height[i]);
+            if (stack.peek() == height[i]) {
+                stack.pop();
+            }
         }
-
         return ans;
     }
 }
