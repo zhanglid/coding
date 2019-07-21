156d76a28fa409f4c439dd3a5b842dfdc4aeb567
Zhangliang <dzl199401@126.com>
2019-07-21 14:48:46 -0700

    done

--- src/test/java/com/zhangliang/leetcode/MergeSortedArrayTest.java
+++ src/test/java/com/zhangliang/leetcode/MergeSortedArrayTest.java
@@ -13,4 +13,13 @@ public class MergeSortedArrayTest {
         s.merge(input, 3, new int[] { 2, 5, 6 }, 3);
         assertArrayEquals(new int[] { 1, 2, 2, 3, 5, 6 }, input);
     }
+
+    @Test
+    public void testCase2() {
+        MergeSortedArray s = new MergeSortedArray();
+
+        int[] input = new int[] { 1, 2, 4, 5, 6, 0 };
+        s.merge(input, 5, new int[] { 3 }, 1);
+        assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6 }, input);
+    }
 }
