15df5214158cea0fa11a4a4a8b2146e72c12fd20
Zhangliang <dzl199401@126.com>
2019-07-21 14:00:20 -0700

    done

--- src/main/java/com/zhangliang/leetcode/MultiplyStrings.java
+++ src/main/java/com/zhangliang/leetcode/MultiplyStrings.java
@@ -20,58 +20,31 @@ You must not use any built-in BigInteger library or convert the inputs to intege
 directly.
 */
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MultiplyStrings {
     public String multiply(String num1, String num2) {
-        if (num1 == null || num2 == null || num1.length() < 1 || num2.length() < 1) {
-            return "";
-        }
-
-        char[] chars1 = num1.toCharArray();
-        char[] chars2 = num2.toCharArray();
-
-        List<Integer> reversedAns = new ArrayList<>();
-        reversedAns.add(0);
-        int offset = 0;
-        for (int i = chars2.length - 1; i >= 0; i--) {
-            int digit = chars2[i] - '0';
-            if (digit != 0) {
-                for (int j = 0; j < chars1.length; j++) {
-                    int digit2 = chars1[chars1.length - j - 1] - '0';
-                    int index = j + offset;
-                    while (index >= reversedAns.size()) {
-                        reversedAns.add(0);
-                    }
-                    reversedAns.set(index, reversedAns.get(index) + digit * digit2);
-                }
-
-                int step = 0;
-                for (int j = 0; j < reversedAns.size(); j++) {
-                    int cur = reversedAns.get(j) + step;
-                    step = cur / 10;
-                    cur %= 10;
-                    reversedAns.set(j, cur);
-                }
-
-                if (step != 0) {
-                    reversedAns.add(step);
-                }
+        int[] digits = new int[num1.length() + num2.length()];
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int targetIndex = (num1.length() - 1 - i) + (num2.length() - 1 - j);
+                digits[targetIndex] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
             }
-            offset++;
+        }
+        int step = 0;
+        // Process step
+        for (int i = 0; i < digits.length; i++) {
+            int total = digits[i] + step;
+            digits[i] = total % 10;
+            step = total / 10;
         }
 
         StringBuilder sb = new StringBuilder();
-        int head = reversedAns.size() - 1;
-        while (head >= 0 && reversedAns.get(head) == 0) {
-            head--;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (sb.length() > 0 || digits[i] != 0) {
+                sb.append(digits[i]);
+            }
         }
-
-        while (head >= 0) {
-            sb.append(reversedAns.get(head--));
-        }
-
-        return sb.length() > 0 ? sb.toString() : "0";
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 }
