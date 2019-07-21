4883b804984a7bfe871cb0f37eee97c139753a05
Zhangliang <dzl199401@126.com>
2019-07-21 15:40:08 -0700

    done

--- src/main/java/com/zhangliang/leetcode/ReadNCharactersGivenRead4.java
+++ src/main/java/com/zhangliang/leetcode/ReadNCharactersGivenRead4.java
@@ -2,64 +2,99 @@ package com.zhangliang.leetcode;
 /*
 The API: int read4(char *buf) reads 4 characters at a time from a file.
 
-The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters 
+The return value is the actual number of characters read. For example, 
+it returns 3 if there is only 3 characters 
 left in the file.
 
-By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
+By using the read4 API, implement the function int read(char *buf, int n) 
+that reads n characters from the file.
 
 Example 1:
 
 Input: buf = "abc", n = 4
 Output: "abc"
 Explanation: The actual number of characters read is 3, which is "abc".
+
 Example 2:
 
 Input: buf = "abcde", n = 5 
 Output: "abcde"
 Note:
 The read function will only be called once for each test case.
+
+Example 3:
+
+Input: file = "abcdABCD1234", n = 12
+Output: 12
+Explanation: After calling your read method, buf should contain 
+"abcdABCD1234". We read a total of 12 characters from the file, 
+so return 12.
+Example 4:
+
+Input: file = "leetcode", n = 5
+Output: 5
+Explanation: After calling your read method, buf should contain "leetc". 
+We read a total of 5 characters from the file, so return 5.
+
 */
 
-public class ReadNCharactersGivenRead4 {
-    private int offset = 0;
+import com.zhangliang.utils.Reader4;
 
-    private int read4(char[] buf) {
-        int nextOffset = Math.min(offset + 4, buf.length);
-        int diff = nextOffset - offset;
-        offset = nextOffset;
-        return diff;
+/**
+ * The read4 API is defined in the parent class Reader4. int read4(char[] buf);
+ */
+public class ReadNCharactersGivenRead4 extends Reader4 {
+    public ReadNCharactersGivenRead4(char[] chars) {
+        super(chars);
     }
 
-    private int cacheSize = 0;
+    // circle buffer
+    private char[] extraBuffer = new char[4];
+    private int extraBufferSize;
+    private int extraBufferStartIndex;
 
     /**
      * @param buf Destination buffer
-     * @param n   Maximum number of characters to read
-     * @return The number of characters read
+     * @param n   Number of characters to read
+     * @return The number of actual characters read
      */
     public int read(char[] buf, int n) {
-        if (buf == null || n < 1) {
+        if (n <= 0 || buf == null) {
             return 0;
         }
-        char[] buffer = new char[4];
-        int count = cacheSize;
         int i = 0;
-        while (count < n) {
-            int num = read4(buffer);
-            count += num;
-            for (int j = 0; i < count && j < num; j++) {
-                buf[i++] = buffer[j];
+        // read from last left
+        if (extraBufferSize > 0) {
+            int j = 0;
+            for (; j < extraBufferSize && i + j < n; j++) {
+                buf[i + j] = extraBuffer[(extraBufferStartIndex + j) % extraBuffer.length];
             }
+            extraBufferSize -= j;
+            i += j;
+            extraBufferStartIndex += j;
+            extraBufferStartIndex %= extraBuffer.length;
+        }
+
+        // read from read4
+        char[] buf4 = new char[4];
+        while (i < n) {
+            int num = read4(buf4);
+            int j = 0;
+            for (; j < num && i + j < n; j++) {
+                buf[i + j] = buf4[j];
+            }
+            if (j < num) {
+                extraBufferSize = num - j;
+                for (int t = 0; t < extraBufferSize; t++) {
+                    extraBuffer[(extraBufferStartIndex + t) % extraBuffer.length] = buf4[j + t];
+                }
+            }
+            i += j;
             if (num < 4) {
                 break;
             }
         }
 
-        if (count > n) {
-            cacheSize = count - n;
-        } else {
-            cacheSize = 0;
-        }
-        return Math.min(n, count);
+        return i;
     }
 }
--- src/test/java/com/zhangliang/leetcode/ReadNCharactersGivenRead4Test.java
+++ src/test/java/com/zhangliang/leetcode/ReadNCharactersGivenRead4Test.java
@@ -1,5 +1,6 @@
 package com.zhangliang.leetcode;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
@@ -7,15 +8,37 @@ public class ReadNCharactersGivenRead4Test {
 
     @Test
     public void testCase() {
-        ReadNCharactersGivenRead4 s = new ReadNCharactersGivenRead4();
-        int ans = s.read(new char[] { 'a', 'b', 'c' }, 4);
-        assertEquals(3, ans);
+        ReadNCharactersGivenRead4 s = new ReadNCharactersGivenRead4("abc".toCharArray());
+        char[] buf = new char[3];
+        int ans = s.read(buf, 4);
+        assertEquals(ans, 3);
+        assertArrayEquals(buf, "abc".toCharArray());
     }
 
     @Test
     public void testCase2() {
-        ReadNCharactersGivenRead4 s = new ReadNCharactersGivenRead4();
-        int ans = s.read(new char[] { 'a', 'b', 'c', 'd', 'e' }, 5);
-        assertEquals(5, ans);
+        ReadNCharactersGivenRead4 s = new ReadNCharactersGivenRead4("abcde".toCharArray());
+        char[] buf = new char[5];
+        int ans = s.read(buf, 5);
+        assertEquals(ans, 5);
+        assertArrayEquals(buf, "abcde".toCharArray());
+    }
+
+    @Test
+    public void testCase3() {
+        ReadNCharactersGivenRead4 s = new ReadNCharactersGivenRead4("abcdABCD1234".toCharArray());
+        char[] buf = new char[12];
+        int ans = s.read(buf, 12);
+        assertEquals(ans, 12);
+        assertArrayEquals(buf, "abcdABCD1234".toCharArray());
+    }
+
+    @Test
+    public void testCase4() {
+        ReadNCharactersGivenRead4 s = new ReadNCharactersGivenRead4("leetcode".toCharArray());
+        char[] buf = new char[5];
+        int ans = s.read(buf, 5);
+        assertEquals(ans, 5);
+        assertArrayEquals(buf, "leetc".toCharArray());
     }
 }
