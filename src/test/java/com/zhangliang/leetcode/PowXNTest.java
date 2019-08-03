42c46f8909b1df56b4a0f693f37098261890b817
Zhangliang <dzl199401@126.com>
2019-08-03 15:19:59 -0700

    done

--- src/test/java/com/zhangliang/leetcode/PowXNTest.java
+++ src/test/java/com/zhangliang/leetcode/PowXNTest.java
@@ -3,15 +3,15 @@ package com.zhangliang.leetcode;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-
 public class PowXNTest {
 
     private static final double epsilon = 1e-6;
+
     @Test
     public void testCase() {
         PowXN s = new PowXN();
         double ans = s.myPow(2, 10);
-        assertEquals(1024.00, ans, Double.MIN_VALUE);
+        assertEquals(1024.00, ans, epsilon);
     }
 
     @Test
@@ -25,13 +25,20 @@ public class PowXNTest {
     public void testCase3() {
         PowXN s = new PowXN();
         double ans = s.myPow(2.0, -2);
-        assertEquals(0.25000, ans, Double.MIN_VALUE);
+        assertEquals(0.25000, ans, epsilon);
     }
 
     @Test
     public void errCase() {
         PowXN s = new PowXN();
         double ans = s.myPow(2.0, -2147483648);
-        assertEquals(0, ans, Double.MIN_VALUE);
+        assertEquals(0, ans, epsilon);
+    }
+
+    @Test
+    public void errCase2() {
+        PowXN s = new PowXN();
+        double ans = s.myPow(-1.0, -2147483648);
+        assertEquals(1, ans, epsilon);
     }
 }
