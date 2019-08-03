5e370d359946f142f9551873872045d406f9fbb4
Zhangliang <dzl199401@126.com>
2019-08-03 14:28:12 -0700

    done

--- src/test/java/com/zhangliang/leetcode/SearchInRotatedSortedArrayTest.java
+++ src/test/java/com/zhangliang/leetcode/SearchInRotatedSortedArrayTest.java
@@ -10,20 +10,27 @@ public class SearchInRotatedSortedArrayTest {
     public void testCase() {
         SearchInRotatedSortedArray s = new SearchInRotatedSortedArray();
         int ans = s.search(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0);
-        assertEquals("default", ans, 4);
+        assertEquals(4, ans);
     }
 
     @Test
     public void testCase2() {
         SearchInRotatedSortedArray s = new SearchInRotatedSortedArray();
         int ans = s.search(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 3);
-        assertEquals("default", ans, -1);
+        assertEquals(-1, ans);
     }
 
     @Test
     public void errCase() {
         SearchInRotatedSortedArray s = new SearchInRotatedSortedArray();
-        int ans = s.search(new int[] {5, 1, 3}, 5);
-        assertEquals("error", ans, 0);
+        int ans = s.search(new int[] { 5, 1, 3 }, 5);
+        assertEquals(0, ans);
+    }
+
+    @Test
+    public void errCase2() {
+        SearchInRotatedSortedArray s = new SearchInRotatedSortedArray();
+        int ans = s.search(new int[] {}, 5);
+        assertEquals(-1, ans);
     }
 }
