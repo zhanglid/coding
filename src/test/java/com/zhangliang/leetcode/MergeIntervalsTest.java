372da3801b279942e4183b3ec4c57f111941e7f0
Zhangliang <dzl199401@126.com>
2019-08-03 15:42:33 -0700

    done

--- src/test/java/com/zhangliang/leetcode/MergeIntervalsTest.java
+++ src/test/java/com/zhangliang/leetcode/MergeIntervalsTest.java
@@ -1,33 +1,34 @@
 package com.zhangliang.leetcode;
 
-import static org.junit.Assert.assertEquals;
-import java.util.List;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MergeIntervalsTest {
     @Test
     public void testCase() {
         MergeIntervals s = new MergeIntervals();
-        List<Interval> ans = s
-                .merge(Interval.getIntervalsList(new int[][] { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } }));
-        List<Interval> expected = Interval.getIntervalsList(new int[][] { { 1, 6 }, { 8, 10 }, { 15, 18 } });
-        assertEquals(expected.toString(), ans.toString());
+        int[][] ans = s.merge(new int[][] { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } });
+        assertArrayEquals(new int[][] { { 1, 6 }, { 8, 10 }, { 15, 18 } }, ans);
     }
 
     @Test
     public void testCase2() {
         MergeIntervals s = new MergeIntervals();
-        List<Interval> ans = s.merge(Interval.getIntervalsList(new int[][] { { 1, 4 }, { 4, 5 } }));
-        List<Interval> expected = Interval.getIntervalsList(new int[][] { { 1, 5 } });
-        assertEquals(expected.toString(), ans.toString());
+        int[][] ans = s.merge(new int[][] { { 1, 4 }, { 4, 5 } });
+        assertArrayEquals(new int[][] { { 1, 5 } }, ans);
     }
 
     @Test
     public void errCase() {
         MergeIntervals s = new MergeIntervals();
-        List<Interval> ans = s.merge(Interval.getIntervalsList(new int[][] { { 1, 4 }, { 2, 3 } }));
-        List<Interval> expected = Interval.getIntervalsList(new int[][] { { 1, 4 } });
-        assertEquals(expected.toString(), ans.toString());
+        int[][] ans = s.merge(new int[][] { { 1, 4 }, { 2, 3 } });
+        assertArrayEquals(new int[][] { { 1, 4 } }, ans);
+    }
+
+    @Test
+    public void errCase2() {
+        MergeIntervals s = new MergeIntervals();
+        int[][] ans = s.merge(new int[][] {});
+        assertArrayEquals(new int[][] {}, ans);
     }
 }
