9f6edf7ad6514c3c90510eb3f3261330ecb5c2ba
Zhangliang <dzl199401@126.com>
2019-08-04 12:56:00 -0700

    done manacher

--- src/main/java/com/zhangliang/leetcode/LongestPalindromicSubstring.java
+++ src/main/java/com/zhangliang/leetcode/LongestPalindromicSubstring.java
@@ -1,45 +1,56 @@
 package com.zhangliang.leetcode;
 /*
-Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+Given a string s, find the longest palindromic substring in s. 
+You may assume that the maximum length of s is 1000.
 
 Example 1:
-
 Input: "babad"
 Output: "bab"
 Note: "aba" is also a valid answer.
-Example 2:
 
+Example 2:
 Input: "cbbd"
 Output: "bb"
 */
 
 public class LongestPalindromicSubstring {
-    public String longestPalindrome(String s) {
-        if (s == null || s.isEmpty()) {
-            return s;
-        }
-
-        String ans = "";
-        for (int i = 0; i < s.length(); i++) {
-            int len = expand(s, i, i);
-            if (len > ans.length()) {
-                ans = s.substring(i - len / 2, i + len / 2 + 1);
+    private int expand(int i, int j, String s) {
+        while (i >= 0 && j < s.length()) {
+            if (s.charAt(i) != s.charAt(j)) {
+                break;
             }
-            len = expand(s, i, i + 1);
-            if (len > ans.length()) {
-                ans = s.substring(i - len / 2 + 1, i + len / 2 + 1);
+            i--;
+            j++;
+        }
+        return j - i + 1 - 2;
+    }
+
+    public String longestPalindrome(String s) {
+        int[] sizes = new int[s.length() * 2];
+        String ans = "";
+        int pivot = 0;
+        int right = 0;
+
+        for (int i = 0; i < sizes.length; i++) {
+            int l = i / 2;
+            int r = i % 2 == 0 ? l : l + 1;
+            int prevSize = i <= right ? sizes[pivot - (i - pivot)] : -1;
+            if (prevSize > 0 && prevSize / 2 < (right - i) / 2) {
+                sizes[i] = prevSize;
+            } else {
+                int size = expand(l, r, s);
+                if (size > ans.length()) {
+                    ans = s.substring(l - (size - 1) / 2, r + (size + 1) / 2);
+                }
+                int nextRight = i + size - 1;
+                if (nextRight > right) {
+                    pivot = i;
+                    right = nextRight;
+                }
+                sizes[i] = size;
             }
         }
 
         return ans;
-    }
-
-    private int expand(String s, int l, int r) {
-        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-            l--;
-            r++;
-        }
-
-        return r - l - 1;
     }
 }
