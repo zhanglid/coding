e25044f9510f21b1bd26e7b59803d08ada66fb38
Zhangliang <dzl199401@126.com>
2019-08-04 21:51:45 -0700

    done

--- src/main/java/com/zhangliang/leetcode/DecodeWays.java
+++ src/main/java/com/zhangliang/leetcode/DecodeWays.java
@@ -9,12 +9,11 @@ A message containing letters from A-Z is being encoded to numbers using the foll
 Given a non-empty string containing only digits, determine the total number of ways to decode it.
 
 Example 1:
-
 Input: "12"
 Output: 2
 Explanation: It could be decoded as "AB" (1 2) or "L" (12).
-Example 2:
 
+Example 2:
 Input: "226"
 Output: 3
 Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
@@ -22,20 +21,21 @@ Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 
 public class DecodeWays {
     public int numDecodings(String s) {
-        if (s == null || s.length() < 1) {
-            return 1;
+        if (s.length() < 1) {
+            return 0;
         }
-
-        char[] chars = s.toCharArray();
-        int[] dp = new int[chars.length + 1];
-        dp[0] = 1;
-        for (int i = 0; i < chars.length; i++) {
-            dp[i + 1] = chars[i] == '0' ? 0 : dp[i];
-            if (i - 1 >= 0 && (chars[i - 1] != '0' && (10 * (chars[i - 1] - '0') + chars[i] - '0' <= 26))) {
-                dp[i + 1] += dp[i - 1];
+        int[] dp = new int[2];
+        dp[0] = s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 1; i < s.length(); i++) {
+            int num = 0;
+            if (s.charAt(i) != '0') {
+                num += dp[(i - 1) % 2];
             }
+            if (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2' && s.charAt(i) <= '6') {
+                num += i - 2 >= 0 ? dp[(i - 2) % 2] : 1;
+            }
+            dp[i % 2] = num;
         }
-
-        return dp[chars.length];
+        return dp[(s.length() - 1) % 2];
     }
 }
