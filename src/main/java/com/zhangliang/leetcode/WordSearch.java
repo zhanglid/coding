fe90ecf799d0f8fd81ce2e878cad1ae65ed224a2
Zhangliang <dzl199401@126.com>
2019-08-25 16:20:13 -0700

    done

--- src/main/java/com/zhangliang/leetcode/WordSearch.java
+++ src/main/java/com/zhangliang/leetcode/WordSearch.java
@@ -24,26 +24,34 @@ Given word = "ABCB", return false.
 import java.util.*;
 
 public class WordSearch {
-    private static final int[][] dirs = new int[][] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+    static private final int[][] dirs = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
 
-    private int hash(int[] pos, int colNum) {
-        return pos[0] * colNum + pos[1];
+    private int hash(int[] pos, char[][] board) {
+        return pos[0] * board[0].length + pos[1];
     }
 
     private boolean dfs(char[][] board, int[] pos, String word, int index, Set<Integer> visited) {
-        int hashCode = hash(pos, board[0].length);
-        if (visited.contains(hashCode) || board[pos[0]][pos[1]] != word.charAt(index)) {
+        // out of bound
+        if (pos[0] < 0 || pos[0] >= board.length || pos[1] < 0 || pos[1] >= board[0].length) {
             return false;
         }
+        int hashCode = hash(pos, board);
+        // visited
+        if (visited.contains(hashCode)) {
+            return false;
+        }
+        // mismatch
+        if (board[pos[0]][pos[1]] != word.charAt(index)) {
+            return false;
+        }
+        // last one
         if (index == word.length() - 1) {
             return true;
         }
+        // find next
         visited.add(hashCode);
         for (int[] dir : dirs) {
             int[] next = new int[] { pos[0] + dir[0], pos[1] + dir[1] };
-            if (next[0] < 0 || next[0] >= board.length || next[1] < 0 || next[1] >= board[0].length) {
-                continue;
-            }
             if (dfs(board, next, word, index + 1, visited)) {
                 return true;
             }
