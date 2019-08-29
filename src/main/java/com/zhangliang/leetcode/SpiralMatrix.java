e0719153adddca7115af8267e59f1474d078c3e6
Zhangliang Dong <zhangld@amazon.com>
2019-08-28 17:04:38 -0700

    done

--- src/main/java/com/zhangliang/leetcode/SpiralMatrix.java
+++ src/main/java/com/zhangliang/leetcode/SpiralMatrix.java
@@ -4,7 +4,6 @@ Given a matrix of m x n elements (m rows, n columns), return all elements of the
 in spiral order.
 
 Example 1:
-
 Input:
 [
  [ 1, 2, 3 ],
@@ -12,8 +11,8 @@ Input:
  [ 7, 8, 9 ]
 ]
 Output: [1,2,3,6,9,8,7,4,5]
-Example 2:
 
+Example 2:
 Input:
 [
   [1, 2, 3, 4],
@@ -23,46 +22,59 @@ Input:
 Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 */
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class SpiralMatrix {
+    private static final int[][] dirs = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
     public List<Integer> spiralOrder(int[][] matrix) {
+        int i = 0;
+        int j = 0;
+        int di = 0;
         List<Integer> ans = new ArrayList<>();
-        if (matrix == null || matrix.length < 1 || matrix[0] == null || matrix[0].length < 1) {
+        if (matrix == null || matrix.length < 1) {
             return ans;
         }
+        int top = 0;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        int bottom = matrix.length - 1;
 
-        int si = 0;
-        int sj = 0;
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        while (m >= 1 && n >= 1) {
-            for (int j = sj; j < sj + n; j++) {
-                ans.add(matrix[si][j]);
-            }
-
-            for (int i = si + 1; i < si + m; i++) {
-                ans.add(matrix[i][sj + n - 1]);
-            }
-
-            if (m > 1) {
-                for (int j = sj + n - 2; j >= sj; j--) {
-                    ans.add(matrix[si + m - 1][j]);
+        while (ans.size() < matrix.length * matrix[0].length) {
+            ans.add(matrix[i][j]);
+            int ni = i + dirs[di][0];
+            int nj = j + dirs[di][1];
+            int tryCount = 0;
+            while (tryCount < 4 && (ni < top || ni > bottom || nj < left || nj > right)) {
+                switch (di) {
+                case 0:
+                    top++;
+                    break;
+                case 1:
+                    right--;
+                    break;
+                case 2:
+                    bottom--;
+                    break;
+                case 3:
+                    left++;
+                    break;
+                default:
+                    break;
                 }
-            }
-
-            if (n > 1) {
-                for (int i = si + m - 2; i >= si + 1; i--) {
-                    ans.add(matrix[i][sj]);
+                di++;
+                if (di >= 4) {
+                    di -= 4;
                 }
+                ni = i + dirs[di][0];
+                nj = j + dirs[di][1];
+                tryCount++;
             }
-
-            m -= 2;
-            n -= 2;
-            si += 1;
-            sj += 1;
+            if (tryCount >= 4) {
+                break;
+            }
+            i = ni;
+            j = nj;
         }
 
         return ans;
