96dd49fd0cdf433ec4558709198e3e41404ce433
Zhangliang <dzl199401@126.com>
2019-08-29 21:38:20 -0700

    done

--- src/main/java/com/zhangliang/leetcode/BinaryTreeInorderTraversal.java
+++ src/main/java/com/zhangliang/leetcode/BinaryTreeInorderTraversal.java
@@ -15,37 +15,42 @@ Output: [1,3,2]
 Follow up: Recursive solution is trivial, could you do it iteratively?
 */
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 import com.zhangliang.utils.TreeNode;
 
 public class BinaryTreeInorderTraversal {
+    class State {
+        boolean isLeftVisited;
+        TreeNode node;
+
+        public State(TreeNode node) {
+            this.node = node;
+        }
+    }
+
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         if (root == null) {
             return ans;
         }
-
-        Stack<TreeNode> stack = new Stack<>();
-        stack.add(root);
+        Stack<State> stack = new Stack<>();
+        stack.push(new State(root));
         while (!stack.isEmpty()) {
-            TreeNode cur = stack.pop();
-            if (cur == null) {
-                continue;
-            }
-            if (cur.left != null) {
-                TreeNode left = cur.left;
-                cur.left = null;
-                stack.push(cur);
-                stack.push(left);
+            State state = stack.pop();
+            if (!state.isLeftVisited) {
+                state.isLeftVisited = true;
+                stack.push(state);
+                if (state.node.left != null) {
+                    stack.push(new State(state.node.left));
+                }
             } else {
-                ans.add(cur.val);
-                stack.push(cur.right);
+                ans.add(state.node.val);
+                if (state.node.right != null) {
+                    stack.push(new State(state.node.right));
+                }
             }
         }
-
         return ans;
     }
 }
