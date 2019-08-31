6ddf665f991b44706f7d3a634ecb2de4c3e059e9
Zhangliang <dzl199401@126.com>
2019-08-31 15:22:12 -0700

    done

--- src/main/java/com/zhangliang/leetcode/CombinationSum.java
+++ src/main/java/com/zhangliang/leetcode/CombinationSum.java
@@ -10,15 +10,14 @@ Note:
 All numbers (including target) will be positive integers.
 The solution set must not contain duplicate combinations.
 Example 1:
-
 Input: candidates = [2,3,6,7], target = 7,
 A solution set is:
 [
   [7],
   [2,2,3]
 ]
-Example 2:
 
+Example 2:
 Input: candidates = [2,3,5], target = 8,
 A solution set is:
 [
@@ -28,36 +27,27 @@ A solution set is:
 ]
 */
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CombinationSum {
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if (nums == null || nums.length < 1) {
-            return ans;
+    private void dfs(int[] candidates, int start, List<Integer> path, int val, int target, List<List<Integer>> result) {
+        if (val == target) {
+            result.add(new ArrayList<>(path));
+            return;
         }
-        helper(nums, target, new ArrayList<>(), 0, 0, ans);
-        return ans;
+        if (val > target) {
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            path.add(candidates[i]);
+            dfs(candidates, i, path, val + candidates[i], target, result);
+            path.remove(path.size() - 1);
+        }
     }
 
-    private void helper(int[] nums, int target, List<Integer> row, int sum, int start, List<List<Integer>> ans) {
-        if (sum == target) {
-            ans.add(new ArrayList<>(row));
-            return;
-        }
-        if (start >= nums.length || sum > target) {
-            return;
-        }
-
-        for (int i = start; i < nums.length; i++) {
-            if (i != start && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            row.add(nums[i]);
-            helper(nums, target, row, sum + nums[i], i, ans);
-            row.remove(row.size() - 1);
-        }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(candidates, 0, new ArrayList<>(), 0, target, result);
+        return result;
     }
 }
