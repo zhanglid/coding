641081f517505c3d954d1e96136b3d19d7d6308a
Zhangliang <dzl199401@126.com>
2019-08-31 16:28:37 -0700

    done

--- src/main/java/com/zhangliang/leetcode/ContainerWithMostWater.java
+++ src/main/java/com/zhangliang/leetcode/ContainerWithMostWater.java
@@ -11,27 +11,17 @@ Note: You may not slant the container and n is at least 2.
 
 public class ContainerWithMostWater {
     public int maxArea(int[] height) {
-        if (height == null || height.length < 2) {
-            return 0;
-        }
-
+        int l = 0;
+        int r = height.length - 1;
         int ans = 0;
-        int left = 0;
-        int right = height.length - 1;
-        while (left < right) {
-            ans = Math.max(ans, Math.min(height[left], height[right]) * (right - left));
-            if (height[left] < height[right]) {
-                left++;
+        while (l < r) {
+            ans = Math.max(ans, (r - l) * Math.min(height[l], height[r]));
+            if (height[l] < height[r]) {
+                l++;
             } else {
-                right--;
+                r--;
             }
         }
-
         return ans;
-    }
-
-    public static void main(String[] args) {
-        ContainerWithMostWater s = new ContainerWithMostWater();
-        System.out.println(s.maxArea(new int[] { 1, 2 }));
     }
 }
--- src/test/java/com/zhangliang/leetcode/ContainerWithMostWaterTest.java
+++ src/test/java/com/zhangliang/leetcode/ContainerWithMostWaterTest.java
@@ -1,13 +1,14 @@
 package com.zhangliang.leetcode;
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class ContainerWithMostWaterTest{
+public class ContainerWithMostWaterTest {
 
     @Test
     public void testCase() {
         ContainerWithMostWater s = new ContainerWithMostWater();
-        int ans = s.maxArea(new int[]{1, 1});
+        int ans = s.maxArea(new int[] { 1, 1 });
         assertEquals(ans, 1);
     }
 }
