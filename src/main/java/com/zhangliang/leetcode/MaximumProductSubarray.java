e7c28109a09a394457b6e70427579ec5f5f4d114
Zhangliang Dong <zhangld@amazon.com>
2020-01-17 13:16:05 -0800

    Linkedin

--- src/main/java/com/zhangliang/leetcode/MaximumProductSubarray.java
+++ src/main/java/com/zhangliang/leetcode/MaximumProductSubarray.java
@@ -4,12 +4,11 @@ Given an integer array nums, find the contiguous subarray within an array (conta
 has the largest product.
 
 Example 1:
-
 Input: [2,3,-2,4]
 Output: 6
 Explanation: [2,3] has the largest product 6.
-Example 2:
 
+Example 2:
 Input: [-2,0,-1]
 Output: 0
 Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
@@ -17,24 +16,20 @@ Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 
 public class MaximumProductSubarray {
     public int maxProduct(int[] nums) {
-        if (nums == null || nums.length < 1) {
-            return 0;
-        }
-        int[] mins = new int[nums.length + 1];
         int[] maxs = new int[nums.length + 1];
-        mins[0] = 1;
+        int[] mins = new int[nums.length + 1];
         maxs[0] = 1;
-        int ans = nums[0];
-        for (int i = 1; i <= nums.length; i++) {
-            mins[i] = Math.min(mins[i - 1] * nums[i - 1], nums[i - 1]);
-            mins[i] = Math.min(mins[i], maxs[i - 1] * nums[i - 1]);
-
-            maxs[i] = Math.max(maxs[i - 1] * nums[i - 1], nums[i - 1]);
-            maxs[i] = Math.max(maxs[i], mins[i - 1] * nums[i - 1]);
-
-            ans = Math.max(ans, maxs[i]);
+        mins[0] = 1;
+        Integer maxProductValue = null;
+        for (int i = 0; i < nums.length; i++) {
+            maxs[i + 1] = Math.max(mins[i] * nums[i], maxs[i] * nums[i]);
+            maxs[i + 1] = Math.max(maxs[i + 1], nums[i]);
+            mins[i + 1] = Math.min(mins[i] * nums[i], maxs[i] * nums[i]);
+            mins[i + 1] = Math.min(mins[i + 1], nums[i]);
+            if (maxProductValue == null || maxs[i + 1] > maxProductValue) {
+                maxProductValue = maxs[i + 1];
+            }
         }
-
-        return ans;
+        return maxProductValue != null ? maxProductValue : 0;
     }
 }
