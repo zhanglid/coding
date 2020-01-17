48a676ab6b2a0be927b973aa326b13785dd7a856
Zhangliang Dong <zhangld@amazon.com>
2020-01-17 14:20:36 -0800

    Linkedin

--- src/main/java/com/zhangliang/leetcode/TextJustification.java
+++ src/main/java/com/zhangliang/leetcode/TextJustification.java
@@ -1,5 +1,6 @@
 package com.zhangliang.leetcode;
-/*Given an array of words and a width maxWidth, format the text such that each line has exactly maxWidth characters 
+/*
+Given an array of words and a width maxWidth, format the text such that each line has exactly maxWidth characters 
 and is fully (left and right) justified.
 
 You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces 
@@ -39,7 +40,7 @@ Output:
 ]
 Explanation: Note that the last line is "shall be    " instead of "shall     be",
              because the last line must be left-justified instead of fully-justified.
-             Note that the second line is also left-justified becase it contains only one word.
+             Note that the second line is also left-justified because it contains only one word.
 Example 3:
 
 Input:
@@ -57,76 +58,79 @@ Output:
 ]
 */
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TextJustification {
-    private String getSpaces(int num) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < num; i++) {
-            sb.append(' ');
+    class LineBuilder {
+        int maxWidth;
+        // Total length of the built line.
+        int totalLength;
+        // Length of total words.
+        int wordsLength;
+        // Words in this line.
+        List<String> words = new ArrayList<>();
+
+        public LineBuilder(int maxWidth) {
+            this.maxWidth = maxWidth;
         }
 
-        return sb.toString();
-    }
-
-    public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> ans = new ArrayList<>();
-        if (words == null || words.length < 1 || maxWidth < 1) {
-            return ans;
-        }
-
-        List<String> line = new ArrayList<>();
-        int curSize = 0;
-        for (String word : words) {
-            if (curSize > 0 && curSize + 1 + word.length() > maxWidth) {
-                // move to next line
-                int extraSpaceSize = maxWidth - curSize + (line.size() - 1);
-                int wordsCount = line.size();
-                int padSize = wordsCount == 1 ? extraSpaceSize
-                        : (int) Math.ceil((float) extraSpaceSize / (wordsCount - 1));
-                int fullPadCount = wordsCount == 1 ? 1 : extraSpaceSize % (wordsCount - 1);
-                if (fullPadCount == 0) {
-                    fullPadCount = wordsCount;
+        public void addWord(String word) {
+            if (hasSpaceForWord(word)) {
+                words.add(word);
+                if (totalLength > 0) {
+                    totalLength += 1;
                 }
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < Math.max(1, wordsCount - 1); i++) {
-                    sb.append(line.get(i));
-                    if (i < fullPadCount) {
-                        sb.append(getSpaces(padSize));
-                    } else {
-                        sb.append(getSpaces(padSize - 1));
-                    }
-                }
-
-                if (wordsCount != 1) {
-                    sb.append(line.get(line.size() - 1));
-                }
-
-                ans.add(sb.toString());
-                line.clear();
-                line.add(word);
-                curSize = word.length();
-            } else {
-                // append and update size
-                curSize += word.length() + (line.isEmpty() ? 0 : 1);
-                line.add(word);
+                totalLength += word.length();
+                wordsLength += word.length();
             }
         }
 
-        StringBuilder lastLineBuilder = new StringBuilder();
-        for (String s : line) {
-            lastLineBuilder.append(s);
-            lastLineBuilder.append(' ');
-        }
-        while (lastLineBuilder.length() < maxWidth) {
-            lastLineBuilder.append(' ');
+        public boolean hasSpaceForWord(String word) {
+            return ((totalLength > 0 ? 1 : 0) + totalLength) + word.length() <= maxWidth;
         }
 
-        if (lastLineBuilder.length() > maxWidth) {
-            lastLineBuilder.deleteCharAt(lastLineBuilder.length() - 1);
+        private String buildSpace(int count) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < count; i++) {
+                sb.append(" ");
+            }
+            return sb.toString();
         }
-        ans.add(lastLineBuilder.toString());
-        return ans;
+
+        public String build(boolean isLastLine) {
+            int totalSpaceNum = maxWidth - wordsLength;
+            int largeSpaceNum = isLastLine ? 0 : totalSpaceNum % (Math.max(1, words.size() - 1));
+            int spaceWidth = isLastLine ? 1 : totalSpaceNum / Math.max(1, (words.size() - 1));
+            StringBuilder sb = new StringBuilder();
+            for (String word : words) {
+                if (sb.length() >= 1) {
+                    sb.append(buildSpace(spaceWidth + (largeSpaceNum == 0 ? 0 : 1)));
+                    if (largeSpaceNum > 0) {
+                        largeSpaceNum--;
+                    }
+                }
+                sb.append(word);
+            }
+            while (sb.length() < maxWidth) {
+                sb.append(buildSpace(1));
+            }
+            return sb.toString();
+        }
+    }
+
+    public List<String> fullJustify(final String[] words, final int maxWidth) {
+        List<String> result = new ArrayList<>();
+        LineBuilder lb = new LineBuilder(maxWidth);
+        for (String word : words) {
+            if (!lb.hasSpaceForWord(word)) {
+                result.add(lb.build(false));
+                lb = new LineBuilder(maxWidth);
+            }
+            lb.addWord(word);
+        }
+        if (lb.words.size() > 0) {
+            result.add(lb.build(true));
+        }
+        return result;
     }
 }
