2e3e8d81fc2f44bf232297f9252929dcdaa55b27
Zhangliang Dong <zhangld@amazon.com>
2020-01-17 23:04:02 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/PowXN.java
+++ src/main/java/com/zhangliang/leetcode/PowXN.java
@@ -6,6 +6,8 @@ Example 1:
 Input: 2.00000, 10
 Output: 1024.00000
 
+B: 10 => 1010
+
 Example 2:
 Input: 2.10000, 3
 Output: 9.26100
@@ -24,30 +26,20 @@ import java.util.*;
 
 public class PowXN {
     public double myPow(double x, int n) {
-        if (n == 0) {
-            return 1;
-        }
-        int sign = n < 0 ? -1 : 1;
+        boolean isNegative = true;
         if (n > 0) {
+            // Attention: Use negative n to cover overflow.
+            isNegative = false;
             n = -n;
         }
-        int base = -1;
-        Stack<Double> xs = new Stack<>();
-        xs.push(x);
-        while ((base << 1) >= n && (base << 1) >> 1 == base) {
-            x *= x;
-            base = base << 1;
-            xs.push(x);
-        }
         double result = 1;
-        while (!xs.isEmpty()) {
-            x = xs.pop();
-            if (n <= base) {
-                n -= base;
+        while (n != 0) {
+            if (n % 2 == -1) {
                 result *= x;
             }
-            base = base >> 1;
+            x *= x;
+            n /= 2;
         }
-        return sign > 0 ? result : 1.0 / result;
+        return isNegative ? 1.0 / result : result;
     }
 }
