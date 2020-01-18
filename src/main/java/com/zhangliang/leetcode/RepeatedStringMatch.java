d56f578ee484ee2b54d9bfc3dc705b434d8ee385
Zhangliang Dong <zhangld@amazon.com>
2020-01-18 09:03:43 -0800

    Google

--- src/main/java/com/zhangliang/leetcode/RepeatedStringMatch.java
+++ src/main/java/com/zhangliang/leetcode/RepeatedStringMatch.java
@@ -14,47 +14,20 @@ The length of A and B will be between 1 and 10000.
 
 public class RepeatedStringMatch {
     public int repeatedStringMatch(String A, String B) {
-        if (A == null || B == null) {
-            return -1;
+        String repeatedA = A;
+        int times = 1;
+        while (repeatedA.length() < B.length()) {
+            times++;
+            repeatedA += A;
         }
-
-        if (B.isEmpty()) {
-            return 0;
+        if (repeatedA.indexOf(B) >= 0) {
+            return times;
         }
-
-        if (A.indexOf(B) >= 0) {
-            return 1;
-        } else if ((A + A).indexOf(B) >= 0) {
-            return 2;
+        repeatedA += A;
+        times++;
+        if (repeatedA.indexOf(B) >= 0) {
+            return times;
         }
-
-        if (B.length() < A.length()) {
-            return -1;
-        }
-
-        int start = B.indexOf(A);
-
-        if (start < 0) {
-            return -1;
-        }
-
-        String left = B.substring(0, start);
-        int num = 1;
-        start += A.length();
-        while (start + A.length() <= B.length() && B.substring(start, start + A.length()).equals(A)) {
-            num++;
-            start += A.length();
-        }
-        String right = B.substring(start);
-
-        if (left.length() >= A.length() || !A.substring(A.length() - left.length(), A.length()).equals(left)) {
-            return -1;
-        }
-
-        if (right.length() >= A.length() || !A.substring(0, right.length()).equals(right)) {
-            return -1;
-        }
-
-        return num + (left.isEmpty() ? 0 : 1) + (right.isEmpty() ? 0 : 1);
+        return -1;
     }
 }
