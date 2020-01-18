69c6326b99c95db6225928f27d9364eaa4c25ef5
Zhangliang Dong <zhangld@amazon.com>
2020-01-17 23:49:34 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/SerializeAndDeserializeBinaryTree.java
+++ src/main/java/com/zhangliang/leetcode/SerializeAndDeserializeBinaryTree.java
@@ -1,12 +1,12 @@
 package com.zhangliang.leetcode;
 /*
-Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored 
-in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or 
-another computer environment.
+Serialization is the process of converting a data structure or object into a sequence of 
+bits so that it can be stored in a file or memory buffer, or transmitted across a network connection 
+link to be reconstructed later in the same or another computer environment.
 
 Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your 
-serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a 
-string and this string can be deserialized to the original tree structure.
+serialization/deserialization algorithm should work. You just need to ensure that a binary tree can 
+be serialized to a string and this string can be deserialized to the original tree structure.
 
 Example: 
 
@@ -27,7 +27,6 @@ should be stateless.
 */
 
 import java.util.*;
-
 import com.zhangliang.utils.TreeNode;
 
 public class SerializeAndDeserializeBinaryTree {
@@ -36,60 +35,73 @@ public class SerializeAndDeserializeBinaryTree {
      * left; TreeNode right; TreeNode(int x) { val = x; } }
      */
     static class Codec {
+        private void levelScan(TreeNode node, List<Integer> result) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            int nonNullSize = node == null ? 0 : 1;
+            queue.add(node);
+            while (!queue.isEmpty() && nonNullSize > 0) {
+                TreeNode tn = queue.poll();
+                result.add(tn == null ? null : tn.val);
+                nonNullSize--;
+                if (tn != null) {
+                    queue.add(tn.left);
+                    if (tn.left != null) {
+                        nonNullSize = queue.size();
+                    }
+                    queue.add(tn.right);
+                    if (tn.right != null) {
+                        nonNullSize = queue.size();
+                    }
+                }
+            }
+        }
 
         // Encodes a tree to a single string.
         public String serialize(TreeNode root) {
             List<Integer> result = new ArrayList<>();
-            Queue<TreeNode> queue = new LinkedList<>();
-            queue.add(root);
-            while (!queue.isEmpty()) {
-                TreeNode node = queue.poll();
-                result.add(node == null ? null : node.val);
-                if (node != null) {
-                    queue.add(node.left);
-                    queue.add(node.right);
-                }
-            }
-            while (!result.isEmpty() && result.get(result.size() - 1) == null) {
-                result.remove(result.size() - 1);
-            }
-            return result.toString();
+            levelScan(root, result);
+            return result.toString().replace(" ", "");
         }
 
-        private TreeNode decode(String part) {
-            if (part.equals("null")) {
+        private TreeNode decodeToken(String token) {
+            if (token.equals("null")) {
                 return null;
             }
-            return new TreeNode(Integer.parseInt(part));
+            return new TreeNode(Integer.parseInt(token));
         }
 
         // Decodes your encoded data to tree.
         public TreeNode deserialize(String data) {
-            data = data.replace(" ", "");
-            data = data.replace("[", "");
-            data = data.replace("]", "");
-            String[] parts = data.split(",");
-            if (data.equals("")) {
+            data = data.substring(1, data.length() - 1);
+            if (data.length() < 1) {
                 return null;
             }
-            TreeNode root = decode(parts[0]);
+            String[] tokens = data.split(",");
+            if (tokens.length < 1) {
+                return null;
+            }
+            TreeNode[] decodedTokens = new TreeNode[tokens.length];
+            for (int i = 0; i < tokens.length; i++) {
+                decodedTokens[i] = decodeToken(tokens[i]);
+            }
             Queue<TreeNode> queue = new LinkedList<>();
-            queue.add(root);
-            for (int i = 1; i < parts.length; i++) {
+            if (decodedTokens[0] == null) {
+                return null;
+            }
+            int index = 0;
+            queue.add(decodedTokens[index++]);
+            while (!queue.isEmpty()) {
                 TreeNode node = queue.poll();
-                node.left = decode(parts[i]);
+                node.left = index < decodedTokens.length ? decodedTokens[index++] : null;
+                node.right = index < decodedTokens.length ? decodedTokens[index++] : null;
                 if (node.left != null) {
                     queue.add(node.left);
                 }
-                if (i + 1 < parts.length) {
-                    i++;
-                    node.right = decode(parts[i]);
-                    if (node.right != null) {
-                        queue.add(node.right);
-                    }
+                if (node.right != null) {
+                    queue.add(node.right);
                 }
             }
-            return root;
+            return decodedTokens[0];
         }
     }
 
