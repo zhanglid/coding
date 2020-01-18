0d4d0450e0c62a33d7bb89ca2a7ee0d12600432d
Zhangliang Dong <zhangld@amazon.com>
2020-01-18 01:31:30 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/MergeKSortedLists.java
+++ src/main/java/com/zhangliang/leetcode/MergeKSortedLists.java
@@ -14,32 +14,31 @@ Input:
 Output: 1->1->2->3->4->4->5->6
 */
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class MergeKSortedLists {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode dummy = new ListNode(-1);
-        ListNode cur = dummy;
         PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
             public int compare(ListNode a, ListNode b) {
                 return a.val - b.val;
             }
         });
-        for (ListNode node : lists) {
+        for (ListNode list : lists) {
+            if (list != null) {
+                pq.add(list);
+            }
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            cur.next = new ListNode(node.val);
+            cur = cur.next;
+            node = node.next;
             if (node != null) {
                 pq.add(node);
             }
         }
-        while (!pq.isEmpty()) {
-            cur.next = pq.poll();
-            cur = cur.next;
-            if (cur.next != null) {
-                pq.add(cur.next);
-                cur.next = null;
-            }
-        }
-
         return dummy.next;
     }
 }
