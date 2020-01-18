814c03c890ca19101427f64bea58ae25c101082c
Zhangliang Dong <zhangld@amazon.com>
2020-01-18 00:01:23 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/SymmetricTree.java
+++ src/main/java/com/zhangliang/leetcode/SymmetricTree.java
@@ -1,6 +1,7 @@
 package com.zhangliang.leetcode;
 /*
-Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+Given a binary tree, check whether it is a mirror of itself 
+(ie, symmetric around its center).
 
 For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
 
@@ -19,69 +20,46 @@ Note:
 Bonus points if you could solve it both recursively and iteratively.
 */
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import com.zhangliang.utils.TreeNode;
 
 public class SymmetricTree {
-    public boolean isSymmetric(TreeNode root) {
-        if (root == null || (root.left == null && root.right == null)) {
-            return true;
-        }
-        if (root.left == null || root.right == null) {
-            return false;
-        }
-
-        if (root.left.val != root.right.val) {
-            return false;
-        }
-
-        TreeNode dummy1 = new TreeNode(0);
-        TreeNode dummy2 = new TreeNode(0);
-
-        dummy1.left = root.left.left;
-        dummy1.right = root.right.right;
-
-        dummy2.left = root.left.right;
-        dummy2.right = root.right.left;
-
-        return isSymmetric(dummy1) && isSymmetric(dummy2);
-    }
-
-    public boolean isSymmetric2(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-
-        List<TreeNode> level = new LinkedList<>();
-        level.add(root);
-
-        while (!level.isEmpty()) {
-            int size = level.size();
-            int l = 0;
-            int r = size - 1;
-            while (l < r) {
-                if (level.get(l) == null && level.get(r) != null || level.get(r) == null && level.get(l) != null
-                        || level.get(l) != null && level.get(r) != null && level.get(l).val != level.get(r).val) {
+    private boolean checkIsSymmetric(List<TreeNode> nodeList) {
+        int r = nodeList.size() - 1;
+        int l = 0;
+        while (l < r) {
+            if (nodeList.get(l) == null || nodeList.get(r) == null) {
+                if (nodeList.get(l) != nodeList.get(r)) {
                     return false;
                 }
-                l++;
-                r--;
+            } else if (nodeList.get(l).val != nodeList.get(r).val) {
+                return false;
             }
-            List<TreeNode> nextLevel = new LinkedList<>();
-            for (TreeNode node : level) {
-                if (node == null) {
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        List<TreeNode> level = new ArrayList<>();
+        level.add(root);
+        while (!level.isEmpty()) {
+            if (!checkIsSymmetric(level)) {
+                return false;
+            }
+            int size = level.size();
+            List<TreeNode> nextLevel = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                if (level.get(i) == null) {
                     continue;
                 }
-                nextLevel.add(node.left);
-
-                nextLevel.add(node.right);
+                nextLevel.add(level.get(i).left);
+                nextLevel.add(level.get(i).right);
             }
-
             level = nextLevel;
         }
-
         return true;
     }
 }
--- src/test/java/com/zhangliang/leetcode/SymmetricTreeTest.java
+++ src/test/java/com/zhangliang/leetcode/SymmetricTreeTest.java
@@ -1,7 +1,6 @@
 package com.zhangliang.leetcode;
 
 import static org.junit.Assert.assertEquals;
-import java.util.Arrays;
 
 import com.zhangliang.utils.TreeNode;
 
@@ -12,14 +11,14 @@ public class SymmetricTreeTest {
     @Test
     public void testCase() {
         SymmetricTree s = new SymmetricTree();
-        boolean ans = s.isSymmetric2(new TreeNode(new Integer[] { 1, 2, 2, 3, 4, 4, 3 }));
+        boolean ans = s.isSymmetric(new TreeNode(new Integer[] { 1, 2, 2, 3, 4, 4, 3 }));
         assertEquals(true, ans);
     }
 
     @Test
     public void testCase2() {
         SymmetricTree s = new SymmetricTree();
-        boolean ans = s.isSymmetric2(new TreeNode(new Integer[] { 1, 2, 2, null, 3, null, 3 }));
+        boolean ans = s.isSymmetric(new TreeNode(new Integer[] { 1, 2, 2, null, 3, null, 3 }));
         assertEquals(false, ans);
     }
 }
