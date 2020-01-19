45c8ca4ba818bb3a3ebce1f1d6bd0542885fcdae
Zhangliang Dong <zhangld@amazon.com>
2020-01-18 23:48:55 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/TopKFrequentElements.java
+++ src/main/java/com/zhangliang/leetcode/TopKFrequentElements.java
@@ -12,37 +12,41 @@ Output: [1]
 Note:
 
 You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
-Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+Your algorithm's time complexity must be better than O(n log n), 
+where n is the array's size.
 */
 
 import java.util.*;
 
 public class TopKFrequentElements {
-    private Map<Integer, Integer> getCounts(int[] nums) {
-        Map<Integer, Integer> counts = new HashMap<>();
+    private Map<Integer, Integer> generateFrequencyMap(int[] nums) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
         for (int num : nums) {
-            counts.put(num, counts.getOrDefault(num, 0) + 1);
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-        return counts;
+        return freqMap;
     }
 
-    public List<Integer> topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> counts = getCounts(nums);
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[1] - b[1];
+    private List<Integer> findTopKFromFreqMap(Map<Integer, Integer> freqMap, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                return freqMap.get(a) - freqMap.get(b);
             }
         });
-        for (Integer key : counts.keySet()) {
-            pq.add(new int[] { key, counts.get(key) });
+
+        for (int key : freqMap.keySet()) {
+            pq.add(key);
             if (pq.size() > k) {
                 pq.poll();
             }
         }
-        List<Integer> ans = new ArrayList<>();
-        while (!pq.isEmpty()) {
-            ans.add(pq.poll()[0]);
-        }
+
+        ArrayList<Integer> ans = new ArrayList<>(pq);
+        Collections.reverse(ans);
         return ans;
+    }
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        return findTopKFromFreqMap(generateFrequencyMap(nums), k);
     }
 }
