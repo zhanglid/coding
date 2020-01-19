3c828fffd48a9aab1f797eabb3af1e6e6b8493e5
Zhangliang Dong <zhangld@amazon.com>
2020-01-19 14:27:31 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/MergeSortedArray.java
+++ src/main/java/com/zhangliang/leetcode/MergeSortedArray.java
@@ -1,14 +1,15 @@
 package com.zhangliang.leetcode;
 /*
-Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as 
-one sorted array.
+Given two sorted integer arrays nums1 and nums2, merge nums2 into 
+nums1 as one sorted array.
 
 Note:
 
-The number of elements initialized in nums1 and nums2 are m and n respectively.
-You may assume that nums1 has enough space (size that is greater or equal to 
-m + n) to hold 
-additional elements from nums2.
+The number of elements initialized in nums1 and nums2 are m and n 
+respectively.
+You may assume that nums1 has enough space (size that is greater or 
+equal to m + n) to hold additional elements from nums2.
+
 Example:
 
 Input:
@@ -20,20 +21,17 @@ Output: [1,2,2,3,5,6]
 
 public class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (nums1 == null || nums2 == null || m < 0 || n < 0) {
-            return;
+        for (int t = nums1.length - 1; t - n >= 0; t--) {
+            nums1[t] = nums1[t - n];
         }
-        // Move num1 offset of n
-        for (int i = m - 1; i >= 0; i--) {
-            nums1[i + n] = nums1[i];
-        }
-        int i1 = n;
-        int i2 = 0;
-        for (int i = 0; i < m + n; i++) {
-            if (i2 >= n || (i1 < m + n) && nums2[i2] > nums1[i1]) {
-                nums1[i] = nums1[i1++];
+        int i = n;
+        int j = 0;
+        int index = 0;
+        while (j < nums2.length) {
+            if (i < nums1.length && nums1[i] < nums2[j]) {
+                nums1[index++] = nums1[i++];
             } else {
-                nums1[i] = nums2[i2++];
+                nums1[index++] = nums2[j++];
             }
         }
     }
