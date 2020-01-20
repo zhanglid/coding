80406ebe276a4b04367282d12d5b38061dd8163e
Zhangliang Dong <zhangld@amazon.com>
2020-01-19 22:42:57 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/SortColors.java
+++ src/main/java/com/zhangliang/leetcode/SortColors.java
@@ -1,9 +1,11 @@
 package com.zhangliang.leetcode;
 /*
-Given an array with n objects colored red, white or blue, sort them in-place so that objects of 
-the same color are adjacent, with the colors in the order red, white and blue.
+Given an array with n objects colored red, white or blue, sort them in-place 
+so that objects of the same color are adjacent, with the colors in the order 
+red, white and blue.
 
-Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue 
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, 
+and blue 
 respectively.
 
 Note: You are not suppose to use the library's sort function for this problem.
@@ -12,32 +14,29 @@ Example:
 
 Input: [2,0,2,1,1,0]
 Output: [0,0,1,1,2,2]
-Follow up:
 
+Follow up:
 A rather straight forward solution is a two-pass algorithm using counting sort.
-First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total 
-number of 0's, then 1's and followed by 2's.
+First, iterate the array counting number of 0's, 1's, and 2's, then overwrite 
+array with total number of 0's, then 1's and followed by 2's.
 Could you come up with a one-pass algorithm using only constant space?
 */
 
 public class SortColors {
     private void swap(int[] nums, int i, int j) {
-        int t = nums[i];
+        int num = nums[i];
         nums[i] = nums[j];
-        nums[j] = t;
+        nums[j] = num;
     }
 
     public void sortColors(int[] nums) {
-        if (nums == null) {
-            return;
-        }
         int l = 0;
         int r = nums.length - 1;
         for (int i = 0; i <= r; i++) {
             if (nums[i] == 0) {
                 swap(nums, l++, i);
             } else if (nums[i] == 2) {
-                swap(nums, r--, i--);
+                swap(nums, i--, r--);
             }
         }
     }
