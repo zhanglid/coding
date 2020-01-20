11a6cfc749f7a856e851da6061d484d881f43a1a
Zhangliang Dong <zhangld@amazon.com>
2020-01-19 17:37:41 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/WordLadder.java
+++ src/main/java/com/zhangliang/leetcode/WordLadder.java
@@ -1,10 +1,12 @@
 package com.zhangliang.leetcode;
 /*
-Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation 
+Given two words (beginWord and endWord), and a dictionary's word list, 
+find the length of shortest transformation 
 sequence from beginWord to endWord, such that:
 
 Only one letter can be changed at a time.
-Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+Each transformed word must exist in the word list. Note that beginWord is 
+not a transformed word.
 Note:
 
 Return 0 if there is no such transformation sequence.
@@ -21,7 +23,8 @@ wordList = ["hot","dot","dog","lot","log","cog"]
 
 Output: 5
 
-Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+Explanation: As one shortest transformation is 
+"hit" -> "hot" -> "dot" -> "dog" -> "cog",
 return its length 5.
 Example 2:
 
@@ -32,51 +35,51 @@ wordList = ["hot","dot","dog","lot","log"]
 
 Output: 0
 
-Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
+Explanation: The endWord "cog" is not in wordList, therefore no possible 
+transformation.
 */
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class WordLadder {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        if (wordList == null || beginWord == null || endWord == null) {
-            return 0;
-        }
-        Set<String> wordsSet = new HashSet<>(wordList);
-        Set<String> achievedSet = new HashSet<>();
-        achievedSet.add(beginWord);
-        int count = 1;
-        while (!achievedSet.contains(endWord)) {
-            if (achievedSet.isEmpty()) {
-                return 0;
-            }
-            Set<String> nextSet = new HashSet<>();
-            for (String s : achievedSet) {
-                StringBuilder sb = new StringBuilder(s);
+    private int bfs(String start, Set<String> words, String end) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add(start);
+        Set<String> visited = new HashSet<>();
+        visited.add(start);
+        int distance = 1;
+        while (!queue.isEmpty() && !visited.contains(end)) {
+            distance++;
+            int size = queue.size();
+            for (int t = 0; t < size; t++) {
+                String node = queue.poll();
+                StringBuilder sb = new StringBuilder(node);
                 for (int i = 0; i < sb.length(); i++) {
-                    char curChar = sb.charAt(i);
-                    for (char c = 'a'; c <= 'z'; c++) {
-                        if (c == curChar) {
-                            continue;
-                        }
-
-                        sb.setCharAt(i, c);
-
-                        String choice = sb.toString();
-                        if (wordsSet.contains(choice)) {
-                            nextSet.add(choice);
-                            wordsSet.remove(choice);
+                    char x = sb.charAt(i);
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        if (ch != x) {
+                            sb.setCharAt(i, ch);
+                            String key = sb.toString();
+                            if (visited.contains(key) || !words.contains(key)) {
+                                continue;
+                            }
+                            visited.add(key);
+                            queue.add(key);
                         }
                     }
-                    sb.setCharAt(i, curChar);
+                    sb.setCharAt(i, x);
                 }
             }
-            achievedSet = nextSet;
-            count++;
         }
 
-        return count;
+        return visited.contains(end) ? distance : 0;
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> dict = new HashSet<>();
+        for (String word : wordList) {
+            dict.add(word);
+        }
+        return bfs(beginWord, dict, endWord);
     }
 }
