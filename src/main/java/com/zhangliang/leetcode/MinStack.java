1f44c7ff810c52a43c462fc29006b23a42d7344f
Zhangliang Dong <zhangld@amazon.com>
2020-01-19 18:37:37 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/MinStack.java
+++ src/main/java/com/zhangliang/leetcode/MinStack.java
@@ -1,6 +1,7 @@
 package com.zhangliang.leetcode;
 /*
-Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+Design a stack that supports push, pop, top, and retrieving the minimum 
+element in constant time.
 
 push(x) -- Push element x onto stack.
 pop() -- Removes the element on top of the stack.
@@ -21,27 +22,29 @@ minStack.getMin();   --> Returns -2.
 import java.util.Stack;
 
 public class MinStack {
-    Stack<Integer> stack;
-    Stack<Integer> minStack;
+
+    Stack<Integer> minStack = new Stack<>();
+    Stack<Integer> stack = new Stack<>();
 
     /** initialize your data structure here. */
     public MinStack() {
-        stack = new Stack<>();
-        minStack = new Stack<>();
+
     }
 
     public void push(int x) {
         stack.push(x);
+        // Attention: check empty min stack.
         if (minStack.isEmpty() || x <= minStack.peek()) {
             minStack.push(x);
         }
     }
 
     public void pop() {
-        int val = stack.pop();
-        if (minStack.peek() == val) {
+        // Attention: Package type should use .equals
+        if (stack.peek().equals(minStack.peek())) {
             minStack.pop();
         }
+        stack.pop();
     }
 
     public int top() {
@@ -51,10 +54,10 @@ public class MinStack {
     public int getMin() {
         return minStack.peek();
     }
-}
 
-/**
- * Your MinStack object will be instantiated and called as such: MinStack obj =
- * new MinStack(); obj.push(x); obj.pop(); int param_3 = obj.top(); int param_4
- * = obj.getMin();
- */
+    /**
+     * Your MinStack object will be instantiated and called as such: MinStack obj =
+     * new MinStack(); obj.push(x); obj.pop(); int param_3 = obj.top(); int param_4
+     * = obj.getMin();
+     */
+}
