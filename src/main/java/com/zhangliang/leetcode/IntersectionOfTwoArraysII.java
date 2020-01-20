7b310f5b7aa62cf79cec7fc8db64b7fefe3403a7
Zhangliang Dong <zhangld@amazon.com>
2020-01-20 11:53:33 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/IntersectionOfTwoArraysII.java
+++ src/main/java/com/zhangliang/leetcode/IntersectionOfTwoArraysII.java
@@ -16,37 +16,37 @@ both arrays. The result can be in any order.
 
 Follow up:
 What if the given array is already sorted? How would you optimize your 
-algorithm?
+algorithm? - Compare and merge.
 What if nums1's size is small compared to nums2's size? Which algorithm 
-is better?
+is better? - Look up and store nums1 in mem.
 What if elements of nums2 are stored on disk, and the memory is limited 
 such that you cannot load all elements into the memory at once?
+- Split the nums2 into each chunk. Calc the intersection of each chunk with
+ the nums1 and store the index of intersected elements in nums1. Merge each result,
+ keep unique index. MapReducer can be used to help this.
 */
 
 import java.util.*;
 
 public class IntersectionOfTwoArraysII {
     public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> dict = new HashMap<>();
+        for (int num : nums1) {
+            dict.put(num, dict.getOrDefault(num, 0) + 1);
+        }
         List<Integer> result = new ArrayList<>();
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int i = 0;
-        int j = 0;
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] == nums2[j]) {
-                result.add(nums1[i]);
-                i++;
-                j++;
-            } else if (nums1[i] < nums2[j]) {
-                i++;
-            } else {
-                j++;
+        for (int num : nums2) {
+            if (dict.containsKey(num)) {
+                result.add(num);
+                dict.put(num, dict.get(num) - 1);
+                if (dict.get(num) == 0) {
+                    dict.remove(num);
+                }
             }
         }
         int[] ans = new int[result.size()];
-        int index = 0;
-        for (int x : result) {
-            ans[index++] = x;
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
         }
         return ans;
     }
