1101058f4e3cf28c43a6d0ac95ed1753918c8642
Zhangliang Dong <zhangld@amazon.com>
2020-01-19 23:54:29 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/InsertInterval.java
+++ src/main/java/com/zhangliang/leetcode/InsertInterval.java
@@ -1,57 +1,56 @@
 package com.zhangliang.leetcode;
 /*
-Given a set of non-overlapping intervals, insert a new interval into the intervals 
-(merge if necessary).
+Given a set of non-overlapping intervals, insert a new interval into the 
+intervals (merge if necessary).
 
-You may assume that the intervals were initially sorted according to their start times.
+You may assume that the intervals were initially sorted according to 
+their start times.
 
 Example 1:
 
 Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
 Output: [[1,5],[6,9]]
+
 Example 2:
 
-Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], 
+newInterval = [4,8]
 Output: [[1,2],[3,10],[12,16]]
 Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
 
 */
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InsertInterval {
-    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-        List<Interval> ans = new ArrayList<>();
-        if (intervals == null || newInterval == null) {
-            return ans;
-        }
+    private boolean hasIntersection(int[] a, int[] b) {
+        return a[1] >= b[0] && a[0] <= b[1];
+    }
 
-        boolean hasNewIntervalMet = false;
-        boolean hasNewIntervalAdded = false;
-        int insertPos = 0;
-        for (int i = 0; i < intervals.size(); i++) {
-            Interval interval = intervals.get(i);
-            if (interval.end < newInterval.start) {
-                insertPos = i + 1;
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        boolean hasInserted = false;
+        for (int[] interval : intervals) {
+            if (hasIntersection(interval, newInterval)) {
+                interval[0] = Math.min(interval[0], newInterval[0]);
+                interval[1] = Math.max(interval[1], newInterval[1]);
+                hasInserted = true;
             }
-            if (interval.end < newInterval.start || interval.start > newInterval.end) {
-                if (hasNewIntervalMet) {
-                    ans.add(newInterval);
-                    hasNewIntervalMet = false;
-                    hasNewIntervalAdded = true;
-                }
-                ans.add(interval);
+            if (!result.isEmpty() && hasIntersection(interval, result.get(result.size() - 1))) {
+                int[] last = result.get(result.size() - 1);
+                last[1] = Math.max(last[1], interval[1]);
             } else {
-                hasNewIntervalMet = true;
-                newInterval.start = Math.min(interval.start, newInterval.start);
-                newInterval.end = Math.max(interval.end, newInterval.end);
+                if (!hasInserted && interval[0] > newInterval[1]) {
+                    result.add(newInterval);
+                    hasInserted = true;
+                }
+                result.add(interval);
             }
         }
-
-        if (!hasNewIntervalAdded) {
-            ans.add(insertPos, newInterval);
+        if (!hasInserted) {
+            result.add(newInterval);
         }
-        return ans;
+
+        return result.toArray(new int[0][2]);
     }
 }
--- src/test/java/com/zhangliang/leetcode/InsertIntervalTest.java
+++ src/test/java/com/zhangliang/leetcode/InsertIntervalTest.java
@@ -1,31 +1,50 @@
 package com.zhangliang.leetcode;
 
 import static org.junit.Assert.assertEquals;
-import java.util.List;
+
+import java.util.*;
 
 import org.junit.Test;
 
 public class InsertIntervalTest {
 
+    private String arrayToString(int[][] nums) {
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = Arrays.toString(nums[i]);
+        }
+
+        return Arrays.toString(strs);
+    }
+
     @Test
     public void testCase() {
         InsertInterval s = new InsertInterval();
-        List<Interval> ans = s.insert(Interval.getIntervalsList(new int[][] { { 1, 3 }, { 6, 9 } }),
-                new Interval(2, 5));
-        assertEquals("[[1, 5], [6, 9]]", ans.toString());
+        int[][] ans = s.insert(new int[][] { { 1, 3 }, { 6, 9 } }, new int[] { 2, 5 });
+        assertEquals("[[1, 5], [6, 9]]", arrayToString(ans));
     }
 
+    // Empty input
     @Test
     public void errCase() {
         InsertInterval s = new InsertInterval();
-        List<Interval> ans = s.insert(Interval.getIntervalsList(new int[][] {}), new Interval(5, 7));
-        assertEquals("[[5, 7]]", ans.toString());
+        int[][] ans = s.insert(new int[][] {}, new int[] { 5, 7 });
+        assertEquals("[[5, 7]]", arrayToString(ans));
     }
 
+    // smaller input
     @Test
     public void errCase2() {
         InsertInterval s = new InsertInterval();
-        List<Interval> ans = s.insert(Interval.getIntervalsList(new int[][] { { 1, 5 } }), new Interval(0, 0));
-        assertEquals("[[0, 0], [1, 5]]", ans.toString());
+        int[][] ans = s.insert(new int[][] { { 1, 5 } }, new int[] { 0, 0 });
+        assertEquals("[[0, 0], [1, 5]]", arrayToString(ans));
+    }
+
+    // inside
+    @Test
+    public void errCase3() {
+        InsertInterval s = new InsertInterval();
+        int[][] ans = s.insert(new int[][] { { 1, 5 } }, new int[] { 2, 3 });
+        assertEquals("[[1, 5]]", arrayToString(ans));
     }
 }
