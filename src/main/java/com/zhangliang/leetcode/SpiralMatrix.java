41ed8576e03141013f6734043d68d08017ad06c3
Zhangliang Dong <zhangld@amazon.com>
2020-01-20 16:25:43 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/SpiralMatrix.java
+++ src/main/java/com/zhangliang/leetcode/SpiralMatrix.java
@@ -1,7 +1,7 @@
 package com.zhangliang.leetcode;
 /*
-Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix 
-in spiral order.
+Given a matrix of m x n elements (m rows, n columns), return all elements of 
+the matrix in spiral order.
 
 Example 1:
 Input:
@@ -25,58 +25,49 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 import java.util.*;
 
 public class SpiralMatrix {
-    private static final int[][] dirs = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+    private static final int[][] DIRS = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
+    private boolean isValidState(int i, int j, int t, int l, int r, int b) {
+        return i >= t && i < b && j >= l && j < r;
+    }
 
     public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix.length < 1 || matrix[0].length < 1) {
+            return result;
+        }
+        // Current window size
+        int t = 0;
+        int l = 0;
+        int r = matrix[0].length;
+        int b = matrix.length;
+
+        // Current cursor
         int i = 0;
         int j = 0;
-        int di = 0;
-        List<Integer> ans = new ArrayList<>();
-        if (matrix == null || matrix.length < 1) {
-            return ans;
-        }
-        int top = 0;
-        int left = 0;
-        int right = matrix[0].length - 1;
-        int bottom = matrix.length - 1;
+        int dirIndex = 0;
 
-        while (ans.size() < matrix.length * matrix[0].length) {
-            ans.add(matrix[i][j]);
-            int ni = i + dirs[di][0];
-            int nj = j + dirs[di][1];
-            int tryCount = 0;
-            while (tryCount < 4 && (ni < top || ni > bottom || nj < left || nj > right)) {
-                switch (di) {
-                case 0:
-                    top++;
-                    break;
-                case 1:
-                    right--;
-                    break;
-                case 2:
-                    bottom--;
-                    break;
-                case 3:
-                    left++;
-                    break;
-                default:
-                    break;
+        result.add(matrix[i][j]);
+        while (result.size() < matrix.length * matrix[0].length) {
+            while (!isValidState(i + DIRS[dirIndex][0], j + DIRS[dirIndex][1], t, l, r, b)) {
+                dirIndex = (dirIndex + 1) % DIRS.length;
+                int[] dir = DIRS[dirIndex];
+                if (dir[0] < 0) {
+                    b += dir[0];
+                } else {
+                    t += dir[0];
                 }
-                di++;
-                if (di >= 4) {
-                    di -= 4;
+                if (dir[1] < 0) {
+                    r += dir[1];
+                } else {
+                    l += dir[1];
                 }
-                ni = i + dirs[di][0];
-                nj = j + dirs[di][1];
-                tryCount++;
             }
-            if (tryCount >= 4) {
-                break;
-            }
-            i = ni;
-            j = nj;
+            i += DIRS[dirIndex][0];
+            j += DIRS[dirIndex][1];
+            result.add(matrix[i][j]);
         }
 
-        return ans;
+        return result;
     }
 }
