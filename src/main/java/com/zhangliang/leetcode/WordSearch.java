5b60df96da46e132e694cb895b330dba87330a60
Zhangliang Dong <zhangld@amazon.com>
2020-01-23 00:06:21 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/WordSearch.java
+++ src/main/java/com/zhangliang/leetcode/WordSearch.java
@@ -3,9 +3,8 @@ package com.zhangliang.leetcode;
 Given a 2D board and a word, find if the word exists in the grid.
 
 The word can be constructed from letters of sequentially adjacent cell, 
-where "adjacent" cells are those 
-horizontally or vertically neighboring. The same letter cell may not be 
-used more than once.
+where "adjacent" cells are those horizontally or vertically neighboring. 
+The same letter cell may not be used more than once.
 
 Example:
 
@@ -24,42 +23,45 @@ Given word = "ABCB", return false.
 import java.util.*;
 
 public class WordSearch {
-    static int[][] DIRS = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-
     private int hash(int i, int j, char[][] board) {
-        return i * board[0].length + j;
+        return j + i * board[0].length;
     }
 
-    private boolean dfs(char[][] board, String target, int index, int i, int j, Set<Integer> visited) {
-        int hashCode = hash(i, j, board);
-        if (visited.contains(hashCode)) {
+    private static final int[][] DIRS = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
+    private boolean dfs(char[][] board, int i, int j, int start, String word, Set<Integer> visited) {
+        if (start >= word.length()) {
             return false;
         }
-        if (board[i][j] != target.charAt(index)) {
+        int key = hash(i, j, board);
+        if (visited.contains(key)) {
             return false;
         }
-        if (index == target.length() - 1) {
+        if (board[i][j] != word.charAt(start)) {
+            return false;
+        }
+        if (start == word.length() - 1) {
             return true;
         }
-        visited.add(hashCode);
+        visited.add(key);
         for (int[] dir : DIRS) {
             int ni = i + dir[0];
             int nj = j + dir[1];
             if (ni < 0 || ni >= board.length || nj < 0 || nj >= board[0].length) {
                 continue;
             }
-            if (dfs(board, target, index + 1, ni, nj, visited)) {
+            if (dfs(board, ni, nj, start + 1, word, visited)) {
                 return true;
             }
         }
-        visited.remove(hashCode);
+        visited.remove(key);
         return false;
     }
 
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (dfs(board, word, 0, i, j, new HashSet<>())) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, i, j, 0, word, new HashSet<>())) {
                     return true;
                 }
             }
