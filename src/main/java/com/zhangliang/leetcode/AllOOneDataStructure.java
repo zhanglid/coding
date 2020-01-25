032e376c679f67c022f9864336660e070c981759
Zhangliang Dong <zhangld@amazon.com>
2020-01-25 12:02:57 -0800

    LinkedIn

--- src/main/java/com/zhangliang/leetcode/AllOOneDataStructure.java
+++ src/main/java/com/zhangliang/leetcode/AllOOneDataStructure.java
@@ -15,20 +15,19 @@ import java.util.*;
 public class AllOOneDataStructure {
     static class AllOne {
         class Node {
-            int freq;
-            Set<String> keys;
-            Node next;
+            int value;
             Node prev;
+            Node next;
+            Set<String> keySet;
 
-            public Node(int freq) {
-                this.freq = freq;
-                keys = new HashSet<>();
+            public Node(int value) {
+                this.value = value;
+                this.keySet = new HashSet<>();
             }
         }
 
-        private Node head = new Node(-1);
-        private Node tail = head;
-
+        Node head = new Node(-1); // Dummy head node.
+        Node tail = head;
         Map<String, Node> nodeMap = new HashMap<>();
 
         /** Initialize your data structure here. */
@@ -36,49 +35,36 @@ public class AllOOneDataStructure {
 
         }
 
-        private Node createNodeAfter(Node node, int value) {
-            Node next = node.next;
-            node.next = new Node(value);
-            node.next.prev = node;
-            node.next.next = next;
-            if (next != null) {
-                next.prev = node.next;
-            } else {
-                tail = node.next;
-            }
-            return node.next;
-        }
-
-        private void removeNode(Node node) {
-            node.prev.next = node.next;
-            if (node.next != null) {
-                node.next.prev = node.prev;
-            } else {
-                tail = node.prev;
+        private void ensureValueNodeAfter(Node node, int value) {
+            if (node.next == null || node.next.value != value) {
+                Node next = node.next;
+                node.next = new Node(value);
+                node.next.prev = node;
+                node.next.next = next;
+                if (next != null) {
+                    next.prev = node.next;
+                } else {
+                    tail = node.next;
+                }
             }
         }
 
         /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
         public void inc(String key) {
-            if (nodeMap.containsKey(key)) {
-                Node node = nodeMap.get(key);
-                // Move to next
-                node.keys.remove(key);
-                // Create freq node
-                if (node.next == null || node.next.freq != node.freq + 1) {
-                    createNodeAfter(node, node.freq + 1);
-                }
-                node.next.keys.add(key);
-                nodeMap.put(key, node.next);
-                if (node.keys.size() == 0) {
-                    removeNode(node);
-                }
-            } else {
-                if (head.next == null || head.next.freq != 1) {
-                    createNodeAfter(head, 1);
-                }
-                head.next.keys.add(key);
+            if (!nodeMap.containsKey(key)) {
+                ensureValueNodeAfter(head, 1);
+                head.next.keySet.add(key);
                 nodeMap.put(key, head.next);
+            } else {
+                Node node = nodeMap.get(key);
+                ensureValueNodeAfter(node, node.value + 1);
+                node.next.keySet.add(key);
+                node.keySet.remove(key);
+                nodeMap.put(key, node.next);
+                if (node.keySet.size() == 0) {
+                    node.prev.next = node.next;
+                    node.next.prev = node.prev;
+                }
             }
         }
 
@@ -87,42 +73,44 @@ public class AllOOneDataStructure {
          * structure.
          */
         public void dec(String key) {
-            if (!nodeMap.containsKey(key)) {
-                return;
-            }
-            Node node = nodeMap.get(key);
-            node.keys.remove(key);
-            int freq = node.freq - 1;
-            // Attention: Node should be removed when freq == 0;
-            if (freq != 0) {
-                if (node.prev.freq != freq) {
-                    createNodeAfter(node.prev, freq);
-                }
-                node.prev.keys.add(key);
-                nodeMap.put(key, node.prev);
-            } else {
-                nodeMap.remove(key);
-            }
+            // Check whether we have the key.
+            if (nodeMap.containsKey(key)) {
+                // Get the node contains the key.
+                Node node = nodeMap.get(key);
 
-            if (node.keys.size() == 0) {
-                removeNode(node);
+                // We have to move the key.
+                node.keySet.remove(key);
+                nodeMap.remove(key);
+
+                // Only move the key to previous node when value is greater than 1.
+                if (node.value > 1) {
+                    if (node.prev.value != node.value - 1) {
+                        ensureValueNodeAfter(node.prev, node.value - 1);
+                    }
+                    node.prev.keySet.add(key);
+                    nodeMap.put(key, node.prev);
+                }
+
+                // Remove the node if the node becomes empty.
+                if (node.keySet.isEmpty()) {
+                    node.prev.next = node.next;
+                    if (node.next != null) {
+                        node.next.prev = node.prev;
+                    } else {
+                        tail = node.prev;
+                    }
+                }
             }
         }
 
         /** Returns one of the keys with maximal value. */
         public String getMaxKey() {
-            if (head == tail) {
-                return "";
-            }
-            return tail.keys.iterator().next();
+            return tail.keySet.iterator().next();
         }
 
         /** Returns one of the keys with Minimal value. */
         public String getMinKey() {
-            if (head == tail) {
-                return "";
-            }
-            return head.next.keys.iterator().next();
+            return head.next.keySet.iterator().next();
         }
     }
 
