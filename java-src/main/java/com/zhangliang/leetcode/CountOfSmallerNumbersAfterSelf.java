722ac8ba904b3d96ad4eb8437ab9fae106250fed
Zhangliang Dong <zhangld@amazon.com>
2020-02-11 23:29:45 -0800

    Google

--- java-src/main/java/com/zhangliang/leetcode/CountOfSmallerNumbersAfterSelf.java
+++ java-src/main/java/com/zhangliang/leetcode/CountOfSmallerNumbersAfterSelf.java
@@ -14,62 +14,84 @@ To the right of 6 there is 1 smaller element (1).
 To the right of 1 there is 0 smaller element.
 */
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CountOfSmallerNumbersAfterSelf {
+    class Num {
+        int value;
+        int index;
+        int count;
 
-    private void add(int[] array, int index, int delta) {
-        int bitIndex = index + 1;
-
-        while(bitIndex < array.length) {
-            array[bitIndex] += delta;
-            bitIndex += bitIndex & -bitIndex;
+        public Num(int value, int index) {
+            this.value = value;
+            this.index = index;
         }
     }
 
-    private int get(int[] array, int end) {
-        int bitIndex = end + 1;
-        int sum = 0;
-        while(bitIndex > 0) {
-            sum += array[bitIndex];
-            bitIndex -= bitIndex & -bitIndex;
+    private void merge(Num[] nums, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        Num[] left = new Num[n1];
+        Num[] right = new Num[n2];
+
+        for (int i = 0; i < left.length; i++) {
+            left[i] = nums[l + i];
+        }
+        for (int i = 0; i < right.length; i++) {
+            right[i] = nums[m + i + 1];
         }
 
-        return sum;
+        int i = 0, j = 0;
+        int k = l;
+        int count = 0;
+
+        while (i < n1 && j < n2) {
+            if (left[i].value <= right[j].value) {
+                left[i].count += count;
+                nums[k++] = left[i];
+                i++;
+            } else {
+                count++;
+                nums[k++] = right[j];
+                j++;
+            }
+        }
+
+        while (i < n1) {
+            left[i].count += count;
+            nums[k++] = left[i];
+            i++;
+        }
+
+        while (j < n2) {
+            nums[k++] = right[j];
+            j++;
+        }
+
+    }
+
+    private void helper(Num[] nums, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            helper(nums, l, m);
+            helper(nums, m + 1, r);
+
+            merge(nums, l, m, r);
+        }
     }
 
     public List<Integer> countSmaller(int[] nums) {
-        List<Integer> ans = new ArrayList<>();
-        if (nums == null || nums.length < 1) {
-            return ans;
-        }
-
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int x : nums) {
-            count.put(x, count.getOrDefault(x, 0) + 1);
-        }
-
-        List<Integer> keys = new ArrayList<>(count.keySet());
-        Collections.sort(keys);
-        Map<Integer, Integer> rankMap = new HashMap<>();
-        for (int i = 0; i < keys.size(); i++) {
-             rankMap.put(keys.get(i), i);
-        }
-
+        Num[] pNums = new Num[nums.length];
+        List<Integer> result = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            nums[i] = rankMap.get(nums[i]);
+            pNums[i] = new Num(nums[i], i);
+            result.add(0);
         }
-
-        int[] array = new int[nums.length + 1];
-        for (int i = nums.length - 1; i >= 0; i--) {
-            ans.add(0, get(array, nums[i] - 1));
-            add(array, nums[i], 1);
+        helper(pNums, 0, nums.length - 1);
+        for (Num num : pNums) {
+            result.set(num.index, num.count);
         }
-
-        return ans;
+        return result;
     }
 }
