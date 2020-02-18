3361d36a5d4e5e432db5d5dc13004d1938c23548
Zhangliang Dong <zhangld@amazon.com>
2020-02-18 11:40:56 -0800

    Google

--- java-src/main/java/com/zhangliang/leetcode/NumberOfIslands.java
+++ java-src/main/java/com/zhangliang/leetcode/NumberOfIslands.java
@@ -24,39 +24,36 @@ Input:
 Output: 3
 */
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class NumberOfIslands {
-    private static final int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+    private static final int[][] DIRS = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 
-    private void bfs(char[][] grid, int i, int j) {
-        Queue<int[]> queue = new LinkedList<int[]>();
-        queue.add(new int[] { i, j });
+    private void dfs(char[][] grid, int i, int j) {
+        if (grid[i][j] != '1') {
+            return;
+        }
         grid[i][j] = '0';
-        while (!queue.isEmpty()) {
-            int[] pos = queue.poll();
-            for (int[] dir : dirs) {
-                int ni = pos[0] + dir[0];
-                int nj = pos[1] + dir[1];
-                if (ni >= 0 && ni < grid.length && nj >= 0 && nj < grid[0].length && grid[ni][nj] == '1') {
-                    grid[ni][nj] = '0';
-                    queue.add(new int[] { ni, nj });
-                }
+        for (int[] dir : DIRS) {
+            int ni = i + dir[0];
+            int nj = j + dir[1];
+            if (ni < 0 || ni >= grid.length || nj < 0 || nj >= grid[0].length) {
+                continue;
             }
+            dfs(grid, ni, nj);
         }
     }
 
     public int numIslands(char[][] grid) {
-        int ans = 0;
+        int count = 0;
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
-                    ans++;
-                    bfs(grid, i, j);
+                    count++;
+                    dfs(grid, i, j);
                 }
             }
         }
-        return ans;
+        return count;
     }
 }
