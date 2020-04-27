d0ec7740a3098b5737b83ea3222af4833d859fee
Zhangliang <dzl199401@126.com>
2020-04-26 17:48:48 -0700

    Google

---
+++ java-src/main/java/com/zhangliang/google/DesignTopK.java
@@ -0,0 +1,17 @@
+package com.zhangliang.google;
+/*
+  @TODO: Google
+  写一种数据结构有findRank(k) 和 add(num)两种方法，findRank就是找到第k个大的数子，
+  add就是加数字进去。很简单的topk方法，讨论了很多种。值得注意的是加重复数字的case
+  https://www.cs.yale.edu/homes/aspnes/pinewiki/OrderStatisticsTree.html
+*/
+
+public class DesignTopK {
+  public int findRank(int k) {
+
+  }
+
+  public void add(int num) {
+
+  }
+}
---
+++ java-src/test/java/com/zhangliang/google/DesignTopKTest.java
@@ -0,0 +1,15 @@
+package com.zhangliang.google;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class DesignTopKTest {
+
+    @Test
+    public void testCase() {
+        DesignTopK s = new DesignTopK();
+        String ans = s.solve();
+        assertEquals("", ans);
+    }
+}
