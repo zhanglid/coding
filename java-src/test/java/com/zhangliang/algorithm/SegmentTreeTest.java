3eded58d8952fe30ddb022bc1eb465d7aae92045
Zhangliang <dzl199401@126.com>
2020-04-26 19:14:40 -0700

    Google

---
+++ java-src/test/java/com/zhangliang/algorithm/SegmentTreeTest.java
@@ -0,0 +1,15 @@
+package com.zhangliang.algorithm;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class SegmentTreeTest {
+
+    @Test
+    public void testCase() {
+        SegmentTree s = new SegmentTree();
+        String ans = s.solve();
+        assertEquals("", ans);
+    }
+}
