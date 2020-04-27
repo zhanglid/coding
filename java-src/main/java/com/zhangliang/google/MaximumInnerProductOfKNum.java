d2d6d4ed9cfb07f3bdba22509823cd82bad59154
Zhangliang <dzl199401@126.com>
2020-04-26 19:35:14 -0700

    Google

---
+++ java-src/main/java/com/zhangliang/google/MaximumInnerProductOfKNum.java
@@ -0,0 +1,13 @@
+package com.zhangliang.google;
+/*
+  @TODO: Google
+  一个大小为N的数组 取K个数 每次从最左或最右取 求sum max
+   然后follow up是取出的K个数需要和一个长为K的给定数组做inner product 求max
+   我傻了上来就DP, 其实第一问brute force更高效。。
+*/
+
+public class MaximumInnerProductOfKNum {
+  public int solve() {
+
+  }
+}
---
+++ java-src/test/java/com/zhangliang/google/MaximumInnerProductOfKNumTest.java
@@ -0,0 +1,15 @@
+package com.zhangliang.google;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class MaximumInnerProductOfKNumTest {
+
+    @Test
+    public void testCase() {
+        MaximumInnerProductOfKNum s = new MaximumInnerProductOfKNum();
+        String ans = s.solve();
+        assertEquals("", ans);
+    }
+}
