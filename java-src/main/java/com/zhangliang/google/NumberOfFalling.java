cf7a573fd80910c828a0c1f07727e77e1027253c
Zhangliang Dong <zhangld@amazon.com>
2020-04-27 16:31:05 -0700

    Google

---
+++ java-src/main/java/com/zhangliang/google/NumberOfFalling.java
@@ -0,0 +1,23 @@
+package com.zhangliang.google;
+/*
+@TODO: Google
+  xxxxx在偶数轮可以滚落1, 3或者4个stairs，奇数轮可以滚1, 2或者4 stairs，然后中间会有一些sticky stairs，
+  掉到sticky stairs的时候不能move小球直接挂掉。问的是从这个n个stairs高的楼梯，
+  小球有多少种方法能够到达ground，也就是stair为0的这一层，超过的话，比如-3，-2，-1也算是到达ground。
+  一开始用dfs的暴力解，后面想用dp做，做到一半的时候时间没了。画了个图，大概长这样。
+  (B)
+  --+
+  3 |
+    +--+
+    2 |
+      +--+
+        1 |
+  --------+---------
+  用dp做，但我用dfs的暴力解，最后要用dp做的时候时间不够了
+*/
+
+public class NumberOfFalling {
+  public int solve() {
+
+  }
+}
---
+++ java-src/test/java/com/zhangliang/google/NumberOfFallingTest.java
@@ -0,0 +1,15 @@
+package com.zhangliang.google;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class NumberOfFallingTest {
+
+    @Test
+    public void testCase() {
+        NumberOfFalling s = new NumberOfFalling();
+        String ans = s.solve();
+        assertEquals("", ans);
+    }
+}
