6a8608414ee7f3780ba58a384459ac4e4052a853
Zhangliang Dong <zhangld@amazon.com>
2020-04-27 11:34:56 -0700

    Google

---
+++ java-src/main/java/com/zhangliang/algorithm/BinarySearchTree.java
@@ -0,0 +1,11 @@
+package com.zhangliang.algorithm;
+/*
+  @TODO: Google
+  Implement the binary search tree
+*/
+
+public class BinarySearchTree {
+  public int solve() {
+
+  }
+}
---
+++ java-src/test/java/com/zhangliang/algorithm/BinarySearchTreeTest.java
@@ -0,0 +1,15 @@
+package com.zhangliang.algorithm;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class BinarySearchTreeTest {
+
+    @Test
+    public void testCase() {
+        BinarySearchTree s = new BinarySearchTree();
+        String ans = s.solve();
+        assertEquals("", ans);
+    }
+}
