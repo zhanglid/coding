7db439f73e22bf7a16ea7e804a5ca5f4d2b503d0
Zhangliang Dong <zhangld@amazon.com>
2020-04-27 14:49:34 -0700

    Google

---
+++ java-src/main/java/com/zhangliang/google/QuickSelectTopK.java
@@ -0,0 +1,13 @@
+package com.zhangliang.google;
+/*
+  @TODO: Google
+  Given an unsorted array, find the c closest elements to k
+  找到和k最接近的c个值
+  using quick select
+*/
+
+public class QuickSelectTopK {
+  public int solve() {
+
+  }
+}
---
+++ java-src/test/java/com/zhangliang/google/QuickSelectTopKTest.java
@@ -0,0 +1,15 @@
+package com.zhangliang.google;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class QuickSelectTopKTest {
+
+    @Test
+    public void testCase() {
+        QuickSelectTopK s = new QuickSelectTopK();
+        String ans = s.solve();
+        assertEquals("", ans);
+    }
+}
