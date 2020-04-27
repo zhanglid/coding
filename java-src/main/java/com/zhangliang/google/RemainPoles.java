c4e056254a627a2ced12f37648ec534f84ef8f65
Zhangliang Dong <zhangld@amazon.com>
2020-04-27 16:44:59 -0700

    Google

---
+++ java-src/main/java/com/zhangliang/google/RemainPoles.java
@@ -0,0 +1,20 @@
+package com.zhangliang.google;
+/*
+@TODO: Google
+  假设给了一堆不同高度的电线杆，现在要在这些电线杆上面放一根电线，绕在他们上面后拉直，问哪些杆子会被碰到。注意两个电线杆子中间如果哪根杆子高度不够，可能就不会被碰到。
+  比如：
+        ｜
+        ｜
+        ｜
+    ｜｜
+  ｜｜｜
+  ｜｜｜｜
+  ｜｜｜｜
+  只有第一根，第三根和最后一根柱子会被电线碰到。注意input是对应index上电线杆的高度，return array/list 包含被碰到的电线杆index
+*/
+
+public class RemainPoles {
+  public int solve() {
+
+  }
+}
---
+++ java-src/test/java/com/zhangliang/google/RemainPolesTest.java
@@ -0,0 +1,15 @@
+package com.zhangliang.google;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class RemainPolesTest {
+
+    @Test
+    public void testCase() {
+        RemainPoles s = new RemainPoles();
+        String ans = s.solve();
+        assertEquals("", ans);
+    }
+}
