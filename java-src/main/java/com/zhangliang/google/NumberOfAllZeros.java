8e055b05f9adbda92d2f4bc27ed0428f4bd2ce1a
Zhangliang <dzl199401@126.com>
2020-04-26 20:51:12 -0700

    Google

---
+++ java-src/main/java/com/zhangliang/google/NumberOfAllZeros.java
@@ -0,0 +1,13 @@
+package com.zhangliang.google;
+/*
+  @TODO: Google
+  是给一个字符串只有1，0，count substring only contains 0.。。比如 10011000 就是0，0，00，0，0，0，00，00，000 返回9
+  1. follow up是 给一个matrix count submatrix only contains 0.
+  2. 2D 里面求array
+*/
+
+public class NumberOfAllZeros {
+  public int solve() {
+
+  }
+}
---
+++ java-src/test/java/com/zhangliang/google/NumberOfAllZerosTest.java
@@ -0,0 +1,14 @@
+package com.zhangliang.google;
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class NumberOfAllZerosTest{
+
+@Test
+public void testCase() {
+    NumberOfAllZeros s = new NumberOfAllZeros();
+    String ans = s.solve();
+    assertEquals("", ans);
+}
+}
