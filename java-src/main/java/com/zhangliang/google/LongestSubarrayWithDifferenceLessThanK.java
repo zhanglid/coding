19963b71a685e67231711291eb082d1fad781802
Zhangliang <dzl199401@126.com>
2020-04-26 20:38:38 -0700

    Google

---
+++ java-src/main/java/com/zhangliang/google/LongestSubarrayWithDifferenceLessThanK.java
@@ -0,0 +1,11 @@
+package com.zhangliang.google;
+/*
+  @TODO: Google
+  就是给一个list的numbers，长度是n，找出最长的sublist, 在这个sublist中，没有差值是大于d的两个数。
+*/
+
+public class LongestSubarrayWithDifferenceLessThanK {
+  public int solve() {
+
+  }
+}
