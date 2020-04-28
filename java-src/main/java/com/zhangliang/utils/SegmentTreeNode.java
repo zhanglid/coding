dc8b27b1159b9e1f3fbb39d11455d79a9bde9708
Zhangliang Dong <zhangld@amazon.com>
2020-04-27 17:26:29 -0700

    Google

--- java-src/main/java/com/zhangliang/utils/SegmentTreeNode.java
+++ java-src/main/java/com/zhangliang/utils/SegmentTreeNode.java
@@ -6,6 +6,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+/**
+ * @TODO: Google
+ */
 public class SegmentTreeNode {
     public int start, end;
     public int value;
