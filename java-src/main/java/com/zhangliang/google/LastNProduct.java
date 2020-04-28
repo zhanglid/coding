276ad7975bc6df7427982a876eeae422f508766d
Zhangliang Dong <zhangld@amazon.com>
2020-04-27 17:10:51 -0700

    Google

---
+++ java-src/main/java/com/zhangliang/google/LastNProduct.java
@@ -0,0 +1,24 @@
+package com.zhangliang.google;
+/*
+  第一輪:   很 nice 的國人小哥
+  寫一個class裡面有兩個method   add() and get()
+  add(int num) : takes an integer
+  get() : output the product of the last N numbers
+  N is given, which can be the argument of the constructor
+  For example:
+  N is 3
+  add(2)
+  add(3)
+  add(4)
+  get() -> return 2 X 3 X 4 = 24
+  add(5)
+  get() -> return 3 X 4 X 5 = 60
+  
+  follow up: 可以做到兩個method 都是 O(1) 嗎?
+*/
+
+public class LastNProduct {
+  public int solve() {
+
+  }
+}
