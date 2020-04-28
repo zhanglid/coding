044d0d9fc444f4a47bec166e598051bcce8894b2
Zhangliang Dong <zhangld@amazon.com>
2020-04-27 20:34:39 -0700

    Google

---
+++ java-src/main/java/com/zhangliang/google/ArrayCover.java
@@ -0,0 +1,14 @@
+package com.zhangliang.google;
+/*
+  @TODO: Google
+  判断覆盖网络需要的最少点数给一个数组每个位置代表当前位置放路由器
+  能覆盖的范围 （idx+-a[idx]），
+  求最少要放几个点能覆盖整个范围，
+  比如a=[1, 1, 2, 1, 1] 答案是1 放在a[2]即可覆盖.
+*/
+
+public class ArrayCover {
+  public int solve() {
+
+  }
+}
---
+++ java-src/test/java/com/zhangliang/google/ArrayCoverTest.java
@@ -0,0 +1,15 @@
+package com.zhangliang.google;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class ArrayCoverTest {
+
+    @Test
+    public void testCase() {
+        ArrayCover s = new ArrayCover();
+        String ans = s.solve();
+        assertEquals("", ans);
+    }
+}
