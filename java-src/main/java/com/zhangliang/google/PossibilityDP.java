3c319ec1dcf3a22ad987760cb74f54035770b86c
Zhangliang Dong <zhangld@amazon.com>
2020-04-27 20:44:42 -0700

    Google

---
+++ java-src/main/java/com/zhangliang/google/PossibilityDP.java
@@ -0,0 +1,15 @@
+package com.zhangliang.google;
+/*
+  问n个球放进k个bucket使得所有bucket都非空，有几种方法。
+  我给了一个计算公式，面试官问我的公式做了什么假设。
+  想了想发现是球和bucket都不是distinguishable。
+  他又问我如果球是distinguishable，或者bucket是distinguishable，
+  或者两者都是，分别要怎么计算。然后再让我选一种实现打印所有放法。
+  follow up是怎么样进行性能优化。
+*/
+
+public class PossibilityDP {
+  public int solve() {
+
+  }
+}
---
+++ java-src/test/java/com/zhangliang/google/PossibilityDPTest.java
@@ -0,0 +1,15 @@
+package com.zhangliang.google;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class PossibilityDPTest {
+
+    @Test
+    public void testCase() {
+        PossibilityDP s = new PossibilityDP();
+        String ans = s.solve();
+        assertEquals("", ans);
+    }
+}
