48ae92e29f6b96734cff90771bd456017cff9f2a
Zhangliang <dzl199401@126.com>
2020-04-27 22:06:20 -0700

    Google

---
+++ java-src/test/java/com/zhangliang/algorithm/ReservoirSamplingTest.java
@@ -0,0 +1,14 @@
+package com.zhangliang.algorithm;
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class ReservoirSamplingTest{
+
+@Test
+public void testCase() {
+    ReservoirSampling s = new ReservoirSampling();
+    String ans = s.solve();
+    assertEquals("", ans);
+}
+}
