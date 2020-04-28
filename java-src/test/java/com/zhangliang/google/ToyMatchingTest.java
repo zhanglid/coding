a9281c2a17b6c70d7f13835514c9db8c0de97a7e
Zhangliang <dzl199401@126.com>
2020-04-27 22:21:00 -0700

    Google

---
+++ java-src/test/java/com/zhangliang/google/ToyMatchingTest.java
@@ -0,0 +1,15 @@
+package com.zhangliang.google;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class ToyMatchingTest {
+
+    @Test
+    public void testCase() {
+        ToyMatching s = new ToyMatching();
+        String ans = s.solve();
+        assertEquals("", ans);
+    }
+}
