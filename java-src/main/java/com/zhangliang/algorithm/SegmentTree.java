692519001623fa31e921c02c1e6693e3bead1231
Zhangliang <dzl199401@126.com>
2020-05-02 14:37:41 -0700

    Google

--- java-src/main/java/com/zhangliang/algorithm/SegmentTree.java
+++ java-src/main/java/com/zhangliang/algorithm/SegmentTree.java
@@ -1,17 +1,155 @@
 package com.zhangliang.algorithm;
 /*
-@TODO: Google
-  Assumptions: 
-  Approach: 
-  DataStructure: 
-  Complexity: 
-      Time: 
-      Space: 
-  !Remeber to comment 
+
+  Example
+  Given start=0, end=3. The segment tree will be:
+
+                [0,  3]
+              /        \
+        [0,  1]           [2, 3]
+        /     \           /     \
+    [0, 0]  [1, 1]     [2, 2]  [3, 3]
+  Given start=1, end=6. The segment tree will be:
+
+                [1,  6]
+              /        \
+        [1,  3]           [4,  6]
+        /     \           /     \
+    [1, 2]  [3,3]     [4, 5]   [6,6]
+    /    \           /     \
+  [1,1]   [2,2]     [4,4]   [5,5]
+
+  https://leetcode.com/articles/a-recursive-approach-to-segment-trees-range-sum-queries-lazy-propagation/
 */
 
-public class SegmentTree {
-  public int solve() {
+interface Merger {
+  public int merge(int a, int b);
 
+  public int rangeUpdate(int value, int delta, int lo, int hi);
+}
+
+public class SegmentTree {
+  int size;
+  int[] tree;
+  int[] lazy;
+  Merger merger;
+
+  private int getTreeSize(int size) {
+    int numOfLeaves = (int) Math.pow(2, Math.ceil(Math.log(size) / Math.log(2)));
+    return 2 * numOfLeaves - 1;
+  }
+
+  private void build(int ti, int lo, int hi, int[] arr) {
+    if (lo > hi) {
+      return;
+    }
+    if (lo == hi) {
+      tree[ti] = arr[lo];
+      return;
+    }
+    int mid = lo + (hi - lo) / 2;
+    int li = 2 * ti + 1;
+    int ri = 2 * ti + 2;
+    build(li, lo, mid, arr);
+    build(ri, mid + 1, hi, arr);
+    tree[ti] = merger.merge(tree[li], tree[ri]);
+  }
+
+  public SegmentTree(int[] arr, Merger merger) {
+    size = arr.length;
+    int treeSize = getTreeSize(size);
+    tree = new int[treeSize];
+    lazy = new int[treeSize];
+    this.merger = merger;
+    build(0, 0, size - 1, arr);
+  }
+
+  private void resolveLazy(int ti, int tl, int tr) {
+    int li = 2 * ti + 1;
+    int ri = 2 * ti + 2;
+
+    if (lazy[ti] != 0) {
+      tree[ti] = merger.rangeUpdate(tree[ti], lazy[ti], tl, tr);
+      if (li < tree.length) {
+        lazy[li] += lazy[ti];
+      }
+      if (ri < tree.length) {
+        lazy[ri] += lazy[ti];
+      }
+      lazy[ti] = 0;
+    }
+  }
+
+  private int queryUtil(int ti, int tl, int tr, int lo, int hi) {
+    if (lo > hi || lo > tr || hi < tl) {
+      return 0;
+    }
+
+    int mid = tl + (tr - tl) / 2;
+    int li = 2 * ti + 1;
+    int ri = 2 * ti + 2;
+
+    resolveLazy(ti, tl, tr);
+
+    if (tl >= lo && tr <= hi) {
+      return tree[ti];
+    }
+    return merger.merge(queryUtil(li, tl, mid, lo, hi), queryUtil(ri, mid + 1, tr, lo, hi));
+  }
+
+  public int query(int lo, int hi) {
+    return queryUtil(0, 0, size - 1, lo, hi);
+  }
+
+  private void updateUtil(int ti, int tl, int tr, int index, int value) {
+    if (index < tl || index > tr) {
+      return;
+    }
+
+    int mid = tl + (tr - tl) / 2;
+    int li = 2 * ti + 1;
+    int ri = 2 * ti + 2;
+
+    resolveLazy(ti, tl, tr);
+
+    if (tl == tr && tl == index) {
+      tree[ti] = value;
+      return;
+    }
+
+    updateUtil(li, tl, mid, index, value);
+    updateUtil(ri, mid + 1, tr, index, value);
+
+    tree[ti] = merger.merge(tree[li], tree[ri]);
+  }
+
+  public void update(int index, int value) {
+    updateUtil(0, 0, size - 1, index, value);
+  }
+
+  private void updateRangeUtil(int ti, int tl, int tr, int lo, int hi, int delta) {
+    if (lo > hi || hi < tl || lo > tr) {
+      return;
+    }
+
+    resolveLazy(ti, tl, tr);
+
+    if (tl >= lo && tr <= hi) {
+      lazy[ti] += delta;
+      resolveLazy(ti, tl, tr);
+      return;
+    }
+
+    int mid = tl + (tr - tl) / 2;
+    int li = 2 * ti + 1;
+    int ri = 2 * ti + 2;
+
+    updateRangeUtil(li, tl, mid, lo, hi, delta);
+    updateRangeUtil(ri, mid + 1, tr, lo, hi, delta);
+    tree[ti] = merger.merge(tree[li], tree[ri]);
+  }
+
+  public void updateRange(int lo, int hi, int delta) {
+    updateRangeUtil(0, 0, size - 1, lo, hi, delta);
   }
 }
--- java-src/test/java/com/zhangliang/algorithm/SegmentTreeTest.java
+++ java-src/test/java/com/zhangliang/algorithm/SegmentTreeTest.java
@@ -1,15 +1,58 @@
 package com.zhangliang.algorithm;
 
 import static org.junit.Assert.assertEquals;
-import java.util.Arrays;
 import org.junit.Test;
 
 public class SegmentTreeTest {
 
+    int[] values = new int[] { 1, 6, 3, 5, 4, 2 };
+    Merger maxMerger = new Merger() {
+        public int merge(int a, int b) {
+            return Math.max(a, b);
+        }
+
+        public int rangeUpdate(int value, int delta, int lo, int hi) {
+            return value + delta;
+        }
+    };
+
+    Merger sumMerger = new Merger() {
+        public int merge(int a, int b) {
+            return a + b;
+        }
+
+        public int rangeUpdate(int value, int delta, int lo, int hi) {
+            return value + delta * (hi - lo + 1);
+        }
+    };
+
     @Test
-    public void testCase() {
-        SegmentTree s = new SegmentTree();
-        String ans = s.solve();
-        assertEquals("", ans);
+    public void buildAndQuery() {
+        SegmentTree maxS = new SegmentTree(values, maxMerger);
+        SegmentTree sumS = new SegmentTree(values, sumMerger);
+        int ans = maxS.query(0, 3);
+        assertEquals(6, ans);
+        ans = sumS.query(0, 3);
+        assertEquals(15, ans);
+    }
+
+    @Test
+    public void update() {
+        SegmentTree maxS = new SegmentTree(values, maxMerger);
+        SegmentTree sumS = new SegmentTree(values, sumMerger);
+        maxS.update(2, 99);
+        sumS.update(2, 99);
+        assertEquals(99, maxS.query(0, 3));
+        assertEquals(111, sumS.query(0, 3));
+    }
+
+    @Test
+    public void updateRange() {
+        SegmentTree maxS = new SegmentTree(values, maxMerger);
+        SegmentTree sumS = new SegmentTree(values, sumMerger);
+        maxS.updateRange(0, 3, 99);
+        sumS.updateRange(0, 3, 99);
+        assertEquals(105, maxS.query(0, 3));
+        assertEquals(411, sumS.query(0, 3));
     }
 }
