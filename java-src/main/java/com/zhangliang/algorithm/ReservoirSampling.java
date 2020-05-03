b581acc74ba9c65d8cd4a9d73d1bb538fdae04c2
Zhangliang <dzl199401@126.com>
2020-05-03 14:50:09 -0700

    Google

--- java-src/main/java/com/zhangliang/algorithm/ReservoirSampling.java
+++ java-src/main/java/com/zhangliang/algorithm/ReservoirSampling.java
@@ -1,17 +1,33 @@
 package com.zhangliang.algorithm;
 /*
-  @TODO: Google
-  Assumptions: 
-  Approach: 
-  DataStructure: 
-  Complexity: 
-      Time: 
-      Space: 
-  !Remeber to comment 
+所以对于其中每个元素，被保留的概率都为 k / n
 */
 
+import java.util.*;
+
 public class ReservoirSampling {
-    public int solve() {
-        
+  int[] nums;
+  Random rand;
+
+  public ReservoirSampling(int[] nums) {
+    this.nums = nums;
+    rand = new Random();
+  }
+
+  public int[] sample(int k) {
+    int[] res = new int[k];
+    int i = 0;
+    for (; i < k && i < nums.length; i++) {
+      res[i] = nums[i];
     }
+
+    for (; i < nums.length; i++) {
+      int r = rand.nextInt(i + 1);
+      if (r < k) {
+        res[r] = nums[i];
+      }
+    }
+
+    return res;
+  }
 }
--- java-src/test/java/com/zhangliang/algorithm/ReservoirSamplingTest.java
+++ java-src/test/java/com/zhangliang/algorithm/ReservoirSamplingTest.java
@@ -1,14 +1,29 @@
 package com.zhangliang.algorithm;
+
 import static org.junit.Assert.assertEquals;
-import java.util.Arrays;
 import org.junit.Test;
 
-public class ReservoirSamplingTest{
+public class ReservoirSamplingTest {
 
-@Test
-public void testCase() {
-    ReservoirSampling s = new ReservoirSampling();
-    String ans = s.solve();
-    assertEquals("", ans);
-}
+    @Test
+    public void testCase() {
+        ReservoirSampling s = new ReservoirSampling(new int[] { 1, 2, 3, 4, 5 });
+        int[] ans = s.sample(2);
+        assertEquals(2, ans.length);
+    }
+
+    @Test
+    public void testCase2() {
+        ReservoirSampling s = new ReservoirSampling(new int[] { 1, 2, 3, 4, 5 });
+        int count = 3000000;
+        int rest = count;
+        int hit = 0;
+        while (rest-- > 0) {
+            int[] ans = s.sample(2);
+            if (ans[0] == 1 || ans[1] == 1) {
+                hit++;
+            }
+        }
+        assertEquals(0.4, (double) hit / count, 0.001);
+    }
 }
