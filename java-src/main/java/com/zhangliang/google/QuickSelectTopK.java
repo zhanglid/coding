7beec51b9fc2ac96c75736fafef12268503cc339
Zhangliang <dzl199401@126.com>
2020-05-05 09:22:19 -0700

    Google

--- java-src/main/java/com/zhangliang/google/QuickSelectTopK.java
+++ java-src/main/java/com/zhangliang/google/QuickSelectTopK.java
@@ -1,13 +1,42 @@
 package com.zhangliang.google;
 /*
-  @TODO: Google
   Given an unsorted array, find the c closest elements to k
   找到和k最接近的c个值
   using quick select
 */
 
 public class QuickSelectTopK {
-  public int solve() {
+  private void swap(int[] arr, int i, int j) {
+    int t = arr[i];
+    arr[i] = arr[j];
+    arr[j] = t;
+  }
 
+  private int partition(int[] arr, int startIndex, int size, int c) {
+    int pivot = arr[startIndex];
+    int l = startIndex + 1;
+    for (int i = startIndex + 1; i < startIndex + size; i++) {
+      if (Math.abs(arr[i] - c) - Math.abs(pivot - c) <= 0) {
+        swap(arr, i, l++);
+      }
+    }
+    l--;
+    swap(arr, startIndex, l);
+    return l;
+  }
+
+  private int quickTopKthUtil(int[] arr, int startIndex, int size, int target, int k) {
+    int r = partition(arr, startIndex, size, target);
+    if (r - startIndex == k - 1) {
+      return arr[r];
+    } else if (r - startIndex > k - 1) {
+      return quickTopKthUtil(arr, startIndex, r - startIndex, target, k);
+    } else {
+      return quickTopKthUtil(arr, r + 1, size - (r - startIndex + 1), target, k - (r - startIndex + 1));
+    }
+  }
+
+  public int quickTopKth(int[] arr, int target, int k) {
+    return quickTopKthUtil(arr, 0, arr.length, target, k);
   }
 }
--- java-src/test/java/com/zhangliang/google/QuickSelectTopKTest.java
+++ java-src/test/java/com/zhangliang/google/QuickSelectTopKTest.java
@@ -1,7 +1,6 @@
 package com.zhangliang.google;
 
 import static org.junit.Assert.assertEquals;
-import java.util.Arrays;
 import org.junit.Test;
 
 public class QuickSelectTopKTest {
@@ -9,7 +8,14 @@ public class QuickSelectTopKTest {
     @Test
     public void testCase() {
         QuickSelectTopK s = new QuickSelectTopK();
-        String ans = s.solve();
-        assertEquals("", ans);
+        int ans = s.quickTopKth(new int[] { 4, 6, 3, 1, 5, 2 }, 1, 2);
+        assertEquals(2, ans);
+    }
+
+    @Test
+    public void testCase2() {
+        QuickSelectTopK s = new QuickSelectTopK();
+        int ans = s.quickTopKth(new int[] { 4, 6, 3, 1, 5, 2 }, 1, 6);
+        assertEquals(6, ans);
     }
 }
