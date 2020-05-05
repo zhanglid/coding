e2040fb6518dcd564ebbc7bec0982301237fc70e
Zhangliang <dzl199401@126.com>
2020-05-04 23:22:55 -0700

    Google

--- java-src/main/java/com/zhangliang/algorithm/BinarySearchTree.java
+++ java-src/main/java/com/zhangliang/algorithm/BinarySearchTree.java
@@ -1,11 +1,76 @@
 package com.zhangliang.algorithm;
 /*
-  @TODO: Google
   Implement the binary search tree
 */
 
 public class BinarySearchTree {
-  public int solve() {
+  public static class Node {
+    int key;
+    Node left, right;
 
+    public Node(int key) {
+      this.key = key;
+      left = null;
+      right = null;
+    }
   }
+
+  Node root = null;
+
+  private Node insertUtil(Node root, int key) {
+    if (root == null) {
+      return new Node(key);
+    }
+    if (key < root.key) {
+      root.left = insertUtil(root.left, key);
+    } else {
+      root.right = insertUtil(root.right, key);
+    }
+    return root;
+  }
+
+  public void insert(int key) {
+    root = insertUtil(root, key);
+  }
+
+  private Node ceilingUtil(Node root, int key) {
+    if (root == null) {
+      return null;
+    }
+    Node res = null;
+    if (root.key <= key) {
+      res = ceilingUtil(root.right, key);
+    } else {
+      res = ceilingUtil(root.left, key);
+    }
+
+    return res == null && root.key <= key ? root : res;
+  }
+
+  public Integer ceiling(int key) {
+    Node node = ceilingUtil(root, key);
+    return node == null ? null : node.key;
+  }
+
+  private Node floorUtil(Node root, int key) {
+    if (root == null) {
+      return null;
+    }
+
+    Node res = null;
+
+    if (key > root.key) {
+      res = floorUtil(root.right, key);
+    } else {
+      res = floorUtil(root.left, key);
+    }
+
+    return res == null && root.key >= key ? root : res;
+  }
+
+  public Integer floor(int key) {
+    Node node = floorUtil(root, key);
+    return node == null ? null : node.key;
+  }
+
 }
--- java-src/test/java/com/zhangliang/algorithm/BinarySearchTreeTest.java
+++ java-src/test/java/com/zhangliang/algorithm/BinarySearchTreeTest.java
@@ -1,7 +1,6 @@
 package com.zhangliang.algorithm;
 
 import static org.junit.Assert.assertEquals;
-import java.util.Arrays;
 import org.junit.Test;
 
 public class BinarySearchTreeTest {
@@ -9,7 +8,26 @@ public class BinarySearchTreeTest {
     @Test
     public void testCase() {
         BinarySearchTree s = new BinarySearchTree();
-        String ans = s.solve();
-        assertEquals("", ans);
+        s.insert(1);
+        s.insert(5);
+        s.insert(4);
+        s.insert(3);
+        s.insert(2);
+        assertEquals(4, (int) s.ceiling(4));
+        assertEquals(5, (int) s.ceiling(7));
+        assertEquals(null, s.ceiling(0));
+    }
+
+    @Test
+    public void testCase2() {
+        BinarySearchTree s = new BinarySearchTree();
+        s.insert(1);
+        s.insert(5);
+        s.insert(4);
+        s.insert(3);
+        s.insert(2);
+        assertEquals(4, (int) s.floor(4));
+        assertEquals(1, (int) s.floor(0));
+        assertEquals(null, s.floor(6));
     }
 }
