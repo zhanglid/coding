3e13508c0a76df30f98b5cb46044d49699dcd0d1
Zhangliang <dzl199401@126.com>
2020-05-04 22:13:41 -0700

    Google

--- java-src/main/java/com/zhangliang/algorithm/RandomShuffle.java
+++ java-src/main/java/com/zhangliang/algorithm/RandomShuffle.java
@@ -1,17 +1,23 @@
 package com.zhangliang.algorithm;
-/*
-  @TODO: Google
-  Assumptions: 
-  Approach: 
-  DataStructure: 
-  Complexity: 
-      Time: 
-      Space: 
-  !Remeber to comment 
-*/
+
+import java.util.*;
 
 public class RandomShuffle {
-  public int solve() {
+  private Random rand = new Random();
+
+  private void swap(int[] values, int i, int j) {
+    int t = values[i];
+    values[i] = values[j];
+    values[j] = t;
+  }
+
+  public void shuffle(int[] values) {
+    int n = values.length;
+
+    for (int i = n - 1; i >= 0; i--) {
+      int r = rand.nextInt(i + 1);
+      swap(values, i, r);
+    }
 
   }
 }
--- java-src/test/java/com/zhangliang/algorithm/RandomShuffleTest.java
+++ java-src/test/java/com/zhangliang/algorithm/RandomShuffleTest.java
@@ -1,15 +1,14 @@
 package com.zhangliang.algorithm;
 
 import static org.junit.Assert.assertEquals;
-import java.util.Arrays;
 import org.junit.Test;
 
 public class RandomShuffleTest {
-
     @Test
     public void testCase() {
         RandomShuffle s = new RandomShuffle();
-        String ans = s.solve();
-        assertEquals("", ans);
+        int[] input = new int[] { 1, 2, 3, 4, 5 };
+        s.shuffle(input);
+        assertEquals(5, input.length);
     }
 }
