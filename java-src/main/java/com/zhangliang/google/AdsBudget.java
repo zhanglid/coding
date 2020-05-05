8af55d5479ba75462fc3ea5c95db516e17345791
Zhangliang <dzl199401@126.com>
2020-05-05 09:45:17 -0700

    Google

--- java-src/main/java/com/zhangliang/google/AdsBudget.java
+++ java-src/main/java/com/zhangliang/google/AdsBudget.java
@@ -1,16 +1,52 @@
 package com.zhangliang.google;
 /*
-@TODO: Google
-Given a budget (W), buy ads. The costs and rewards of ads on each day are given by array (rewards and costs). If you bought ads on day t, you cannot buy ads on t+1. Find the max reward under budget.
+Given a budget (W), buy ads. The costs and rewards of ads on each day are given by array (rewards and costs). If you bought ads on day t, you cannot buy ads on t+1. Find the max reward under budget. 解法：dynamic programming. max_reward[date][budget] = max(max_reward[date-1][budget], rewards[date] + max_reward[date-2][budget-costs[date])
 
-解法：dynamic programming. max_reward[date][budget] = max(max_reward[date-1][budget], rewards[date] + max_reward[date-2][budget-costs[date])
 
 Follow-up: return the dates of ads that achieved the best reward.
 
 */
 
-public class AdsBudget {
-  public int solve() {
+import java.util.*;
 
+public class AdsBudget {
+  public int solve(int[] rewards, int[] costs, int budget) {
+    int n = costs.length;
+    int[][] dp = new int[n + 1][budget + 1];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j <= budget; j++) {
+        dp[i + 1][j] = dp[i][j];
+        if (costs[i] <= j) {
+          dp[i + 1][j] = Math.max(dp[i + 1][j], rewards[i] + dp[i][j - costs[i]]);
+        }
+      }
+    }
+    return dp[n][budget];
+  }
+
+  public List<Integer> solveFollowUp(int[] rewards, int[] costs, int budget) {
+    int n = costs.length;
+    int[][] dp = new int[n + 1][budget + 1];
+    boolean[][] selected = new boolean[n + 1][budget + 1];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j <= budget; j++) {
+        dp[i + 1][j] = dp[i][j];
+        if (costs[i] <= j) {
+          if (dp[i + 1][j] < rewards[i] + dp[i][j - costs[i]]) {
+            dp[i + 1][j] = rewards[i] + dp[i][j - costs[i]];
+            selected[i + 1][j] = true;
+          }
+        }
+      }
+    }
+    List<Integer> res = new ArrayList<>();
+    int leftBudget = budget;
+    for (int i = n - 1; i >= 0; i--) {
+      if (selected[i][leftBudget]) {
+        leftBudget -= costs[i];
+        res.add(i);
+      }
+    }
+    return res;
   }
 }
--- java-src/test/java/com/zhangliang/google/AdsBudgetTest.java
+++ java-src/test/java/com/zhangliang/google/AdsBudgetTest.java
@@ -1,7 +1,8 @@
 package com.zhangliang.google;
 
 import static org.junit.Assert.assertEquals;
-import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 import org.junit.Test;
 
 public class AdsBudgetTest {
@@ -9,7 +10,19 @@ public class AdsBudgetTest {
     @Test
     public void testCase() {
         AdsBudget s = new AdsBudget();
-        String ans = s.solve();
-        assertEquals("", ans);
+        int[] rewards = new int[] { 1, 2, 3 };
+        int[] costs = new int[] { 1, 1, 1 };
+        assertEquals(3, s.solve(rewards, costs, 1));
+        assertEquals(5, s.solve(rewards, costs, 2));
+
+    }
+
+    @Test
+    public void testCase2() {
+        AdsBudget s = new AdsBudget();
+        int[] rewards = new int[] { 1, 2, 3 };
+        int[] costs = new int[] { 1, 1, 1 };
+        assertArrayEquals(new Integer[] { 2 }, s.solveFollowUp(rewards, costs, 1).toArray(new Integer[0]));
+        assertArrayEquals(new Integer[] { 2, 1 }, s.solveFollowUp(rewards, costs, 2).toArray(new Integer[0]));
     }
 }
