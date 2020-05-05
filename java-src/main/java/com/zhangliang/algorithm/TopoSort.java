6d73130f3c6fb57f2e0e2be8b31652275ad0fdc2
Zhangliang <dzl199401@126.com>
2020-05-05 00:55:19 -0700

    Google

--- java-src/main/java/com/zhangliang/algorithm/TopoSort.java
+++ java-src/main/java/com/zhangliang/algorithm/TopoSort.java
@@ -1,17 +1,112 @@
 package com.zhangliang.algorithm;
 /*
-@TODO: Google
-  Assumptions: 
-  Approach: 
-  DataStructure: 
-  Complexity: 
-      Time: 
-      Space: 
-  !Remeber to comment 
+
 */
 
-public class TopoSort {
-  public int solve() {
+import java.util.*;
 
+public class TopoSort {
+  private Map<Integer, Set<Integer>> buildGraph(int n, int[][] edges) {
+    Map<Integer, Set<Integer>> graph = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+      graph.put(i, new HashSet<>());
+    }
+    for (int[] edge : edges) {
+      graph.get(edge[0]).add(edge[1]);
+    }
+    return graph;
+  }
+
+  public int[] indegreeSort(int n, int[][] edges) {
+    int[] indegree = new int[n];
+    for (int[] edge : edges) {
+      indegree[edge[1]]++;
+    }
+    Map<Integer, Set<Integer>> graph = buildGraph(n, edges);
+    int[] res = new int[n];
+    Queue<Integer> q = new LinkedList<>();
+    for (int i = 0; i < n; i++) {
+      if (indegree[i] == 0) {
+        q.add(i);
+      }
+    }
+    int index = 0;
+    while (!q.isEmpty()) {
+      int node = q.poll();
+      res[index++] = node;
+      for (int next : graph.get(node)) {
+        indegree[next]--;
+        if (indegree[next] == 0) {
+          q.add(next);
+        }
+      }
+    }
+
+    return res;
+  }
+
+  private boolean dfsWithCircleDetect(int node, Stack<Integer> stack, Set<Integer> visited, Set<Integer> path,
+      Map<Integer, Set<Integer>> graph) {
+    if (visited.contains(node)) {
+      return true;
+    }
+    if (path.contains(node)) {
+      return false;
+    }
+    path.add(node);
+    for (int next : graph.get(node)) {
+      if (!dfsWithCircleDetect(next, stack, visited, path, graph)) {
+        return false;
+      }
+    }
+    stack.push(node);
+    visited.add(node);
+    path.remove(node);
+    return true;
+  }
+
+  private void dfs(int node, Stack<Integer> stack, Set<Integer> visited, Map<Integer, Set<Integer>> graph) {
+    if (visited.contains(node)) {
+      return;
+    }
+    for (int next : graph.get(node)) {
+      dfs(next, stack, visited, graph);
+    }
+    stack.push(node);
+    visited.add(node);
+  }
+
+  public int[] dfsSort(int n, int[][] edges) {
+    Map<Integer, Set<Integer>> graph = buildGraph(n, edges);
+    Set<Integer> visited = new HashSet<>();
+
+    Stack<Integer> stack = new Stack<>();
+    for (int i = 0; i < n; i++) {
+      dfs(i, stack, visited, graph);
+    }
+
+    int[] res = new int[stack.size()];
+    for (int i = 0; i < res.length; i++) {
+      res[i] = stack.pop();
+    }
+    return res;
+  }
+
+  public int[] dfsSortWithCircle(int n, int[][] edges) {
+    Map<Integer, Set<Integer>> graph = buildGraph(n, edges);
+    Set<Integer> visited = new HashSet<>();
+
+    Stack<Integer> stack = new Stack<>();
+    for (int i = 0; i < n; i++) {
+      if (!dfsWithCircleDetect(i, stack, visited, new HashSet<>(), graph)) {
+        return null;
+      }
+    }
+
+    int[] res = new int[stack.size()];
+    for (int i = 0; i < res.length; i++) {
+      res[i] = stack.pop();
+    }
+    return res;
   }
 }
--- java-src/test/java/com/zhangliang/algorithm/TopoSortTest.java
+++ java-src/test/java/com/zhangliang/algorithm/TopoSortTest.java
@@ -1,15 +1,69 @@
 package com.zhangliang.algorithm;
 
-import static org.junit.Assert.assertEquals;
-import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.*;
+
 import org.junit.Test;
 
 public class TopoSortTest {
+    private Map<Integer, Set<Integer>> buildGraph(int n, int[][] edges) {
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new HashSet<>());
+        }
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+        }
+        return graph;
+    }
+
+    private boolean checkTopo(int[] order, int n, int[][] edges) {
+        Map<Integer, Set<Integer>> graph = buildGraph(n, edges);
+        for (int i = 1; i < order.length; i++) {
+            int a = order[i - 1];
+            int b = order[i];
+            if (graph.get(b).contains(a)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     @Test
     public void testCase() {
         TopoSort s = new TopoSort();
-        String ans = s.solve();
-        assertEquals("", ans);
+        int n = 6;
+        int[][] edges = new int[][] { { 4, 0 }, { 5, 0 }, { 4, 1 }, { 3, 1 }, { 2, 3 }, { 5, 2 } };
+        int[] ans = s.indegreeSort(n, edges);
+        assertTrue(checkTopo(ans, n, edges));
+    }
+
+    @Test
+    public void testCase2() {
+        TopoSort s = new TopoSort();
+        int n = 6;
+        int[][] edges = new int[][] { { 4, 0 }, { 5, 0 }, { 4, 1 }, { 3, 1 }, { 2, 3 }, { 5, 2 } };
+        int[] ans = s.dfsSort(n, edges);
+        assertTrue(checkTopo(ans, n, edges));
+    }
+
+    @Test
+    public void testCase3() {
+        TopoSort s = new TopoSort();
+        int n = 6;
+        int[][] edges = new int[][] { { 4, 0 }, { 5, 0 }, { 4, 1 }, { 3, 1 }, { 2, 3 }, { 5, 2 } };
+        int[] ans = s.dfsSortWithCircle(n, edges);
+        assertTrue(checkTopo(ans, n, edges));
+    }
+
+    @Test
+    public void testCase4() {
+        TopoSort s = new TopoSort();
+        int n = 6;
+        int[][] edges = new int[][] { { 4, 0 }, { 5, 0 }, { 4, 1 }, { 3, 1 }, { 2, 3 }, { 5, 2 }, { 1, 2 } };
+        int[] ans = s.dfsSortWithCircle(n, edges);
+        assertEquals(null, ans);
     }
 }
