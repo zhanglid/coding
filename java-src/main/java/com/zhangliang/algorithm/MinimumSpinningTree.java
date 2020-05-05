e6e7b3bf3bef6c3a70b99f018dda934338f63178
Zhangliang <dzl199401@126.com>
2020-05-04 23:58:07 -0700

    Google

--- java-src/main/java/com/zhangliang/algorithm/MinimumSpinningTree.java
+++ java-src/main/java/com/zhangliang/algorithm/MinimumSpinningTree.java
@@ -1,10 +1,70 @@
 package com.zhangliang.algorithm;
 /*
-@TODO: Google
+  Minimum Spinning Tree
 */
 
-public class MinimumSpinningTree {
-  public int solve() {
+import java.util.*;
 
+public class MinimumSpinningTree {
+  class UnionFind {
+    private int[] fathers;
+
+    public UnionFind(int size) {
+      fathers = new int[size];
+      for (int i = 0; i < size; i++) {
+        fathers[i] = i;
+      }
+    }
+
+    private int findRoot(int a) {
+      int father = fathers[a];
+
+      if (father == a) {
+        return father;
+      }
+
+      int root = findRoot(father);
+      fathers[a] = root;
+      return root;
+    }
+
+    public boolean isConnect(int a, int b) {
+      int ra = findRoot(a);
+      int rb = findRoot(b);
+
+      return ra == rb;
+    }
+
+    public void connect(int a, int b) {
+      int ra = findRoot(a);
+      int rb = findRoot(b);
+
+      if (ra != rb) {
+        fathers[ra] = rb;
+      }
+    }
+
+  }
+
+  public List<int[]> solve(int n, List<int[]> edges) {
+    UnionFind uf = new UnionFind(n);
+    Collections.sort(edges, new Comparator<int[]>() {
+      public int compare(int[] a, int[] b) {
+        return a[2] - b[2];
+      }
+    });
+
+    List<int[]> res = new ArrayList<>();
+    for (int[] edge : edges) {
+      int a = edge[0];
+      int b = edge[1];
+
+      if (!uf.isConnect(a, b)) {
+        res.add(edge);
+        uf.connect(a, b);
+      }
+    }
+
+    return res;
   }
 }
--- java-src/test/java/com/zhangliang/algorithm/MinimumSpinningTreeTest.java
+++ java-src/test/java/com/zhangliang/algorithm/MinimumSpinningTreeTest.java
@@ -1,7 +1,7 @@
 package com.zhangliang.algorithm;
 
 import static org.junit.Assert.assertEquals;
-import java.util.Arrays;
+import java.util.*;
 import org.junit.Test;
 
 public class MinimumSpinningTreeTest {
@@ -9,7 +9,8 @@ public class MinimumSpinningTreeTest {
     @Test
     public void testCase() {
         MinimumSpinningTree s = new MinimumSpinningTree();
-        String ans = s.solve();
-        assertEquals("", ans);
+        List<int[]> ans = s.solve(5, Arrays.asList(new int[][] { { 0, 1, 1 }, { 0, 2, 7 }, { 1, 2, 5 }, { 1, 3, 4 },
+                { 3, 4, 2 }, { 1, 4, 3 }, { 2, 4, 6 }, { 3, 4, 2 } }));
+        assertEquals("[[0, 1, 1], [3, 4, 2], [1, 4, 3], [1, 2, 5]]", Arrays.deepToString(ans.toArray(new int[0][3])));
     }
 }
