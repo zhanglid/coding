e28bc2f8d962ab48264190865f8fd57cc00b70f6
Zhangliang Dong <zhangld@amazon.com>
2020-05-06 11:38:20 -0700

    Google

--- java-src/main/java/com/zhangliang/google/NumberOfFalling.java
+++ java-src/main/java/com/zhangliang/google/NumberOfFalling.java
@@ -2,9 +2,9 @@ package com.zhangliang.google;
 /*
 @TODO: Google
   xxxxx在偶数轮可以滚落1, 3或者4个stairs，奇数轮可以滚1, 2或者4 stairs，然后中间会有一些sticky stairs，
-  掉到sticky stairs的时候不能move小球直接挂掉。问的是从这个n个stairs高的楼梯，
+  掉到sticky stairs的时候不能move, 小球直接挂掉。问的是从这个n个stairs高的楼梯，
   小球有多少种方法能够到达ground，也就是stair为0的这一层，超过的话，比如-3，-2，-1也算是到达ground。
-  一开始用dfs的暴力解，后面想用dp做，做到一半的时候时间没了。画了个图，大概长这样。
+
   (B)
   --+
   3 |
@@ -14,10 +14,42 @@ package com.zhangliang.google;
         1 |
   --------+---------
   用dp做，但我用dfs的暴力解，最后要用dp做的时候时间不够了
+
+  n -> n - 1
+    -> n - 3
+    -> n - 4
+
+  int[] dp 
+  dp[i] -> how many ways to get level i.
+  from n to 0
 */
 
-public class NumberOfFalling {
-  public int solve() {
+import java.util.*;
 
+public class NumberOfFalling {
+  private static final int[] ODD_JUMP = new int[] { 1, 2, 4 };
+  private static final int[] EVEN_JUMP = new int[] { 1, 3, 4 };
+
+  public int solve(int n, int[] stickyStairs) {
+    int[] dp = new int[n + 1];
+    Set<Integer> sticky = new HashSet<>();
+    for (int stair : stickyStairs) {
+      sticky.add(stair);
+    }
+    dp[n] = 1;
+    for (int i = n; i > 0; i--) {
+      if (sticky.contains(i)) {
+        continue;
+      }
+      for (int step : i % 2 == 0 ? EVEN_JUMP : ODD_JUMP) {
+        int t = Math.max(0, i - step);
+        dp[t] += dp[i];
+        if (t == 0) {
+          break;
+        }
+      }
+    }
+
+    return dp[0];
   }
 }
--- java-src/test/java/com/zhangliang/google/NumberOfFallingTest.java
+++ java-src/test/java/com/zhangliang/google/NumberOfFallingTest.java
@@ -1,15 +1,17 @@
 package com.zhangliang.google;
 
 import static org.junit.Assert.assertEquals;
-import java.util.Arrays;
 import org.junit.Test;
 
 public class NumberOfFallingTest {
 
     @Test
     public void testCase() {
+        /**
+         * 4 -> 3, 1, 0 | 3 -> 2, 1, 0 | 2 -> 1, 0 | 1 -> 0 |
+         */
         NumberOfFalling s = new NumberOfFalling();
-        String ans = s.solve();
-        assertEquals("", ans);
+        int ans = s.solve(4, new int[] { 3 });
+        assertEquals(2, ans);
     }
 }
