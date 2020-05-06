5d00032b5d782be09415a008e907d499eead0928
Zhangliang Dong <zhangld@amazon.com>
2020-05-06 11:25:15 -0700

    Google

--- java-src/main/java/com/zhangliang/google/NumberOfAllZeros.java
+++ java-src/main/java/com/zhangliang/google/NumberOfAllZeros.java
@@ -1,6 +1,5 @@
 package com.zhangliang.google;
 /*
-  @TODO: Google
   是给一个字符串只有1，0，count substring only contains 0.。。
   比如 10011000 就是0，0，00，0，0，0，00，00，000 返回9
   1. follow up是 给一个matrix count submatrix only contains 0.
