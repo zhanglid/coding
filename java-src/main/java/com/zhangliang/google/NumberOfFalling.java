ca584b9b503ffa410cb7ebaaf97743fa205e2a91
Zhangliang Dong <zhangld@amazon.com>
2020-05-06 11:39:25 -0700

    Google

--- java-src/main/java/com/zhangliang/google/NumberOfFalling.java
+++ java-src/main/java/com/zhangliang/google/NumberOfFalling.java
@@ -1,6 +1,5 @@
 package com.zhangliang.google;
 /*
-@TODO: Google
   xxxxx在偶数轮可以滚落1, 3或者4个stairs，奇数轮可以滚1, 2或者4 stairs，然后中间会有一些sticky stairs，
   掉到sticky stairs的时候不能move, 小球直接挂掉。问的是从这个n个stairs高的楼梯，
   小球有多少种方法能够到达ground，也就是stair为0的这一层，超过的话，比如-3，-2，-1也算是到达ground。
