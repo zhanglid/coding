ebfa9702da6b7451619dd96e2ca4a93e757bf55f
Zhangliang <dzl199401@126.com>
2020-05-06 10:50:14 -0700

    Google

--- java-src/main/java/com/zhangliang/google/NumberOfAllZeros.java
+++ java-src/main/java/com/zhangliang/google/NumberOfAllZeros.java
@@ -1,13 +1,27 @@
 package com.zhangliang.google;
 /*
   @TODO: Google
-  是给一个字符串只有1，0，count substring only contains 0.。。比如 10011000 就是0，0，00，0，0，0，00，00，000 返回9
+  是给一个字符串只有1，0，count substring only contains 0.。。
+  比如 10011000 就是0，0，00，0，0，0，00，00，000 返回9
   1. follow up是 给一个matrix count submatrix only contains 0.
   2. 2D 里面求array
 */
 
 public class NumberOfAllZeros {
-  public int solve() {
+  public int countZero(String s) {
+    int res = 0;
+    int c = 0;
+    for (char x : s.toCharArray()) {
+      if (x == '0') {
+        c++;
+      } else {
+        c = 0;
+      }
+      res += c;
+    }
+    return res;
+  }
 
+  public int countSubmatrixZero(int[][] grid) {
   }
 }
--- java-src/test/java/com/zhangliang/google/NumberOfAllZerosTest.java
+++ java-src/test/java/com/zhangliang/google/NumberOfAllZerosTest.java
@@ -1,14 +1,26 @@
 package com.zhangliang.google;
+
 import static org.junit.Assert.assertEquals;
-import java.util.Arrays;
 import org.junit.Test;
 
-public class NumberOfAllZerosTest{
+public class NumberOfAllZerosTest {
 
-@Test
-public void testCase() {
-    NumberOfAllZeros s = new NumberOfAllZeros();
-    String ans = s.solve();
-    assertEquals("", ans);
-}
+    @Test
+    public void testCase() {
+        NumberOfAllZeros s = new NumberOfAllZeros();
+        int ans = s.countZero("10011000");
+        assertEquals(9, ans);
+    }
+
+    @Test
+    public void testCase2() {
+        NumberOfAllZeros s = new NumberOfAllZeros();
+        int ans = s.countSubmatrixZero(new int[][] {
+            {0, 1, 1, 0, 1},
+            {1, 0, 1, 0, 0},
+            {1, 1, 1, 0, 0},
+            {1, 1, 1, 0, 1},
+        });
+        assertEquals(18, ans);
+    }
 }
