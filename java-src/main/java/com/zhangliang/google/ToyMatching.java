5b6fc151f44987ac49a85bd9d3c5ceb25caef969
Zhangliang Dong <zhangld@amazon.com>
2020-05-06 17:02:05 -0700

    Google

--- java-src/main/java/com/zhangliang/google/ToyMatching.java
+++ java-src/main/java/com/zhangliang/google/ToyMatching.java
@@ -4,12 +4,46 @@ package com.zhangliang.google;
 maximum weight bipartite matching?
 匈牙利算法？
 最大权匹
-南亚老头子，就说有一堆玩具，分属于不同的category，并且有红蓝两种颜色，问一种算法让其中玩具两两配对，配对规则 1. category和颜色都最好不一样，并且使这样的对数最大化，2.如果无法满足category或者颜色不一样，那么去掉其中的一个条件，让这样的配对数最大化，3.如果1,2条件都不能满足，那么就都一样的也要配对，输入是一个array，array里面的元素是个tuple（ID，color, category，输出是配好的pair.
-这轮体验不太好，首先题目比较复杂，而且面试官口音很严重，比如pair 他一直在说peer，我是真没搞清楚他让我干啥，然后我说能不能给我一个例子，他也没理我，就一只重复peer，peer，大概花了十五分钟左右才明白了题意。另外题目也说的不是很清楚，这个开始就是说输出category和颜色都不一样的pair，我单纯的认为输出category和颜色都不一样的pair就好了，结果我都要写完了，他就又说要是无法同时满足这两个条件，可以去掉一个条件再配对。还有一个很要命的是这个面试官似乎对python不是很熟悉，我已经在边写边解释给他听了，但是他还是要总叫我停下来，然后他真的毫不夸张的一个word一个word的读我的代码，实在是太托时间了，我的解法也就是暴力解，这样的情况下，我没有写完， 不知道有没有大神有更好的解法？
+南亚老头子，就说有一堆玩具，分属于不同的category，
+并且有红蓝两种颜色，问一种算法让其中玩具两两配对，
+
+配对规则 
+1.category和颜色都最好不一样，并且使这样的对数最大化，
+2.如果无法满足category或者颜色不一样，那么去掉其中的一个条件，
+让这样的配对数最大化，
+3.如果1,2条件都不能满足，那么就都一样的也要配对，
+输入是一个array，array里面的元素是个tuple (ID，color, category)，输出是配好的pair.
+
+(1, CAT1, C1)
+(2, CAT2, C2)
+(3, CAT3, C1)
+
+这轮体验不太好，首先题目比较复杂，而且面试官口音很严重，
+比如pair 他一直在说peer，我是真没搞清楚他让我干啥，
+然后我说能不能给我一个例子，他也没理我，就一只重复peer，peer，
+大概花了十五分钟左右才明白了题意。另外题目也说的不是很清楚，
+这个开始就是说输出category和颜色都不一样的pair，
+我单纯的认为输出category和颜色都不一样的pair就好了，
+结果我都要写完了，他就又说要是无法同时满足这两个条件，可以去掉一个条件再配对。还有一个很要命的是这个面试官似乎对python不是很熟悉，我已经在边写边解释给他听了，但是他还是要总叫我停下来，然后他真的毫不夸张的一个word一个word的读我的代码，实在是太托时间了，我的解法也就是暴力解，这样的情况下，我没有写完， 不知道有没有大神有更好的解法？
 */
 
-public class ToyMatching {
-  public int solve() {
+import java.util.*;
 
+public class ToyMatching {
+  public static class Toy {
+    int id;
+    int category;
+    int color;
+
+    public Toy(int id, int category, int color) {
+      this.id = id;
+      this.category = category;
+      this.color = color;
+    }
+
+  }
+
+  public int solve(List<Toy> toys) {
+    
   }
 }
