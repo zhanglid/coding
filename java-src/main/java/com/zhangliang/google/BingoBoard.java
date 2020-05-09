299c54aed185f2f21ae789377830a4512c2c2b46
Zhangliang <dzl199401@126.com>
2020-05-09 16:42:54 -0700

    Google

--- java-src/main/java/com/zhangliang/google/BingoBoard.java
+++ java-src/main/java/com/zhangliang/google/BingoBoard.java
@@ -1,12 +1,56 @@
 package com.zhangliang.google;
 /*
-生成一个5*5的 bingo board，第一列值在1-15之间， 第二列16-30，。。baord里不能有重复的数。
+@TODO: Google
+生成一个5*5的 bingo board，第一列值在1-15之间， 第二列16-30，。。board里不能有重复的数。
+
 followup是要生成很多board，并且每两个board之间不能有相同的列。也很简单，先生产很多不重复的第一列，
 然后第二列，。。。之后一个一个assemble就好。
 */
 
-public class BingoBoard {
-  public int solve() {
+import java.util.*;
 
+public class BingoBoard {
+
+  private static final int N = 5;
+  private static final int SIZE = 15;
+
+  Random rand = new Random();
+
+  private void swap(int[] nums, int i, int j) {
+    int t = nums[i];
+    nums[i] = nums[j];
+    nums[j] = t;
+  }
+
+  private void shuffle(int[] nums) {
+    for (int i = nums.length - 1; i >= 0; i--) {
+      int l = rand.nextInt(i + 1);
+      swap(nums, l, i);
+    }
+  }
+
+  private int[] randomCol(int offset) {
+    int[] nums = new int[SIZE];
+    for (int i = 0; i < SIZE; i++) {
+      nums[i] = offset + i;
+    }
+    shuffle(nums);
+    int[] res = new int[N];
+    for (int i = 0; i < N; i++) {
+      res[i] = nums[i];
+    }
+    return res;
+  }
+
+  public int[][] generate() {
+    int[][] res = new int[N][N];
+    for (int i = 0; i < res.length; i++) {
+      int[] col = randomCol(i * 15 + 1);
+      for (int j = 0; j < N; j++) {
+        res[i][j] = col[j];
+      }
+    }
+
+    return res;
   }
 }
--- java-src/test/java/com/zhangliang/google/BingoBoardTest.java
+++ java-src/test/java/com/zhangliang/google/BingoBoardTest.java
@@ -1,7 +1,9 @@
 package com.zhangliang.google;
 
 import static org.junit.Assert.assertEquals;
-import java.util.Arrays;
+
+import java.util.*;
+
 import org.junit.Test;
 
 public class BingoBoardTest {
@@ -9,7 +11,7 @@ public class BingoBoardTest {
     @Test
     public void testCase() {
         BingoBoard s = new BingoBoard();
-        String ans = s.solve();
-        assertEquals("", ans);
+        int[][] ans = s.generate();
+        System.out.println(Arrays.deepToString(ans));
     }
 }
